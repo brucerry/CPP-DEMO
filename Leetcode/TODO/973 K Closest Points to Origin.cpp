2acd5a47c041317743d63e82b496349e052cf282
brucerry <bruce.cheung111@gmail.com>
2022-01-31 04:39:05 +0800

    commit

---
+++ Leetcode/TODO/973 K Closest Points to Origin.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/k-closest-points-to-origin/
+
+
+#include <vector>
+using namespace std;
+
+
+
+class Solution {
+public:
+  vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+    
+  }
+};
