46f5d97d6e4fc3cb9545d69e9df07a55f2a1c982
brucerry <bruce.cheung111@gmail.com>
2022-02-01 17:15:00 +0800

    commit

--- Leetcode/673 Number of Longest Increasing Subsequence.cpp
+++ Leetcode/673 Number of Longest Increasing Subsequence.cpp
@@ -3,54 +3,50 @@
 */
 
 #include <vector>
-#include <queue>
 #include <iostream>
+#include <unordered_map>
+#include <tuple>
 using namespace std;
 
 class Solution {
 public:
-  void findNumberOfLIS(vector<int> nums, int start, int prevNum, int len, priority_queue<int> &pq) {
-    if (start == nums.size()) {
-      pq.push(len);
-      return;
-    }
-    
-    pq.push(len);
+  int findNumberOfLIS(vector<int> nums) {
+    unordered_map<int, tuple<int, int>> LIScount;
+    int lenLIS = 0, res = 0;
 
-    if (len == 0) {
-      findNumberOfLIS(nums, start + 1, nums[start], len + 1, pq);
-    } else {
-      for (int i = start; i < nums.size(); i++) {
-        if (prevNum < nums[i]) {
-          findNumberOfLIS(nums, i + 1, nums[i], len + 1, pq);
+    for (int i = nums.size() - 1; i >= 0; i--) {
+      int maxLen = 1, maxCount = 1;
+
+      for (int j = i + 1; j < nums.size(); j++) {
+        if (nums[i] < nums[j]) {
+          auto [ len, count ] = LIScount[j];
+          if (len + 1 > maxLen) {
+            maxLen = len + 1;
+            maxCount = count;
+          } else if (len + 1 == maxLen) {
+            maxCount += count;
+          }
         }
       }
-    }
-  }
 
-  int findNumberOfLIS(vector<int> nums) {
-    priority_queue<int> pq;
-    
-    for (int i = 0; i < nums.size(); i++) {
-      findNumberOfLIS(nums, i, -1, 0, pq);
-    }
+      if (maxLen > lenLIS) {
+        lenLIS = maxLen;
+        res = maxCount;
+      } else if (maxLen == lenLIS) {
+        res += maxCount;
+      }
 
-    int longestLen = pq.top();
-    int count = 0;
-    while (pq.size()) {
-      if (pq.top() == longestLen) count++;
-      else return count;
-      pq.pop();
+      LIScount[i] = { maxLen, maxCount };
     }
 
-    return count;
+    return res;
   }
 };
 
 int main() {
   Solution sol;
-  //cout << sol.findNumberOfLIS({1,3,5,4,7});
-  //cout << sol.findNumberOfLIS({2,2,2,2,2});
-  //cout << sol.findNumberOfLIS({1,2,4,3,5,4,7,2});
-  cout << sol.findNumberOfLIS({1,2,3,1,2,3,1,2,3});
+  cout << sol.findNumberOfLIS({1,3,5,4,7}) << endl;
+  cout << sol.findNumberOfLIS({2,2,2,2,2}) << endl;
+  cout << sol.findNumberOfLIS({1,2,4,3,5,4,7,2}) << endl;
+  cout << sol.findNumberOfLIS({1,2,3,1,2,3,1,2,3}) << endl;
 }
