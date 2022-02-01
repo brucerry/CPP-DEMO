c1eab2629e4b9ed68f5c33fa9052f8534881c09e
brucerry <bruce.cheung111@gmail.com>
2022-02-02 02:18:34 +0800

    ok

---
+++ 10 Common Coding Interview Problems/First and last index in sorted array.cpp
@@ -0,0 +1,65 @@
+/*
+  n = length of nums
+  
+  time: O(log(n))
+  space: O(1)
+*/
+
+#include <iostream>
+#include <vector>
+#include <tuple>
+using namespace std;
+
+int findFirst(vector<int> nums, int target) {
+  if (nums.front() == target) return 0;
+  int left = 0, right = nums.size() - 1;
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (nums[mid] == target) {
+      if (nums[mid - 1] < nums[mid]) return mid;
+      else right = mid - 1;
+    }
+    else if (nums[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+  return -1;
+}
+
+int findEnd(vector<int> nums, int target) {
+  if (nums.back() == target) return nums.size() - 1;
+  int left = 0, right = nums.size() - 1;
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (nums[mid] == target) {
+      if (nums[mid + 1] > nums[mid]) return mid;
+      else left = mid + 1;
+    }
+    else if (nums[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+  return -1;
+}
+
+tuple<int, int> firstAndLast(vector<int> nums, int target) {
+  return { findFirst(nums, target), findEnd(nums, target) };
+}
+
+
+// time: O(n)
+// space: O(1)
+// tuple<int, int> firstAndLast(vector<int> nums, int target) {
+//   int i = 0, j = nums.size() - 1;
+
+//   while ((i < nums.size() || j >= 0) && (i <= j)) {
+//     if (nums[i] == target && nums[j] == target) return { i, j };
+//     if (nums[i] != target) i++;
+//     if (nums[j] != target) j--;
+//   }
+
+//   return { -1, -1 };
+// }
+
+int main() {
+  auto [ i1, i2 ] = firstAndLast({2,4,5,5,5,7,9,10,11,12,12,12,12,12,12,12,12,13,13,13,14,99,100}, 12);
+  cout << i1 << " " << i2;
+}
---
+++ 10 Common Coding Interview Problems/Valid anagram.cpp
@@ -0,0 +1,33 @@
+/*
+  n1 = length of s1
+  n2 = length of s2
+
+  time: O(max(n1, n2))
+  space: O(max(n1, n2))
+*/
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<char, int> charCount(string s) {
+  unordered_map<char, int> count;
+  for (auto ch : s) {
+    count[ch]++;
+  }
+  return count;
+}
+
+bool isAnagram(string s1, string s2) {
+  return charCount(s1) == charCount(s2);
+}
+
+int main() {
+  cout << isAnagram("bookg", "mary") << endl; // false
+  cout << isAnagram("danger", "garden") << endl; // true
+  cout << isAnagram("dangers", "garden") << endl; // false
+  cout << isAnagram("dangerggf", "fggardegn") << endl; // true
+  cout << isAnagram("aaaaaaaaaaaaaaassssssssssssssssssddddd", "dddddssssssssssssssssssaaaaaaaaaaaaaaa") << endl; // true
+  cout << isAnagram("aaaaaaaaaaaaaaassssssssssssFssssssddddd", "dddddssssssssssssssssssaaaaaaaaaaaaaaa") << endl; // false
+}
