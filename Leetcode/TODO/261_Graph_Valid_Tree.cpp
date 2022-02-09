db0637926f916c528a28ccc003c13426f5f59e37
brucerry <bruce.cheung111@gmail.com>
2022-02-09 19:08:20 +0800

    commit from vscode

--- Leetcode/TODO/261_Graph_Valid_Tree.cpp
+++ Leetcode/TODO/261_Graph_Valid_Tree.cpp
@@ -1,15 +1,10 @@
-// no subcription to Leetcode
 // https://leetcode.com/problems/graph-valid-tree/
 
-// alternative website
-// https://algomonster.medium.com/leetcode-261-graph-valid-tree-f27c212c1db1
-
-// youtube
 // https://www.youtube.com/watch?v=bXsUuownnoQ
 
 class Solution {
 public:
-    bool validTree(int n, vector<vector<int>>& edges) {
-        
-    }
+  bool validTree(int n, vector<vector<int>>& edges) {
+    
+  }
 };
---
+++ Leetcode/TODO/269_Alien_Dictionary.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/alien-dictionary/
+
+// https://www.youtube.com/watch?v=6kTZYvNNyps
+
+class Solution {
+public:
+  string alienDictionary(vector<string>& words) {
+    
+  }
+};
