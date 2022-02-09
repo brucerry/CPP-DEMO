b48b7881d54078e08a812d052a17d68c000fb57d
brucerry <bruce.cheung111@gmail.com>
2022-02-09 21:45:06 +0800

    commit from vscode

---
+++ Leetcode/261_Graph_Valid_Tree.cpp
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/graph-valid-tree/
+
+// https://www.lintcode.com/problem/178/
+// https://www.youtube.com/watch?v=bXsUuownnoQ
+
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+
+class Solution {
+private:
+  unordered_map<int, vector<int>> buildGraph(vector<vector<int>> edges) {
+    unordered_map<int, vector<int>> graph;
+    for (auto edge : edges) {
+      int a = edge[0];
+      int b = edge[1];
+      graph[a].push_back(b);
+      graph[b].push_back(a);
+    }
+    return graph;
+  }
+
+  bool exploreAllNodes(unordered_map<int, vector<int>> graph, int node, int prevNode, unordered_set<int> &visited) {
+    if (visited.count(node)) return false;
+    visited.insert(node);
+
+    for (auto neighbor : graph[node]) {
+      if (neighbor != prevNode && !exploreAllNodes(graph, neighbor, node, visited)) return false;
+    }
+
+    return true;
+  }
+
+public:
+  bool validTree(int n, vector<vector<int>> edges) {
+    unordered_map<int, vector<int>> graph = buildGraph(edges);
+    unordered_set<int> visited;
+
+    bool valid = exploreAllNodes(graph, 0, -1, visited);
+
+    return valid && visited.size() == n;
+  }
+};
+
+int main() {
+  Solution sol;
+  cout << sol.validTree(5, {{0, 1}, {0, 2}, {0, 3}, {1, 3}});
+}
---
+++ Leetcode/374_Guess_Number_Higher_or_Lower.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/guess-number-higher-or-lower/
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+  // ternary search
+  // time: O(log(n)) base-3
+  // space: O(1)
+  int guessNumber(int n) {
+    int low = 1;
+    int high = n;
+    while (low <= high) {
+      int mid1 = low + (high - low) / 2;
+      int mid2 = high - (high - low) / 2;
+      int result1 = guess(mid1);
+      int result2 = guess(mid2);
+      if (result1 == 0) return mid1;
+      if (result2 == 0) return mid2;
+      if (result1 == -1) high = mid1 - 1;
+      else if (result2 == 1) low = mid2 + 1;
+      else {
+        low = mid1 + 1;
+        high = mid2 - 1;
+      }
+    }
+    return -1;
+  }
+
+
+  // binary search
+  // time: O(log(n)) base-2
+  // space: O(1)
+  // int guessNumber(int n) {
+  //   int low = 1;
+  //   int high = n;
+  //   while (low <= high) {
+  //     int mid = low + (high - low) / 2;
+  //     int result = guess(mid);
+  //     if (result == -1) high = mid - 1;
+  //     else if (result == 1) low = mid + 1;
+  //     else return mid;
+  //   }
+  //   return -1;
+  // }
+};
