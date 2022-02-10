289752eade703ef846c751e1b4f2b358c119044b
brucerry <bruce.cheung111@gmail.com>
2022-02-10 23:43:41 +0800

    O(n * log(n)) strategy

---
+++ Leetcode/300_Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/longest-increasing-subsequence/
+
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution {
+private:
+  // time: O(n^2)
+  // space: O(n^2)
+  // int lengthOfLIS(vector<int> nums, int prevNum, int idx, unordered_map<string, int> &memo) {
+  //   if (idx == nums.size()) return 0;
+
+  //   string key = to_string(prevNum) + ',' + to_string(idx);
+  //   if (memo.count(key)) return memo[key];
+
+  //   if (prevNum < nums[idx]) {
+  //     memo[key] = max(1 + lengthOfLIS(nums, nums[idx], idx + 1, memo), lengthOfLIS(nums, prevNum, idx + 1, memo));
+  //   } else {
+  //     memo[key] = lengthOfLIS(nums, prevNum, idx + 1, memo);
+  //   }
+
+  //   return memo[key];
+  // }
+
+public:
+  // int lengthOfLIS(vector<int>& nums) {
+  //   unordered_map<string, int> memo;
+  //   return lengthOfLIS(nums, -10001, 0, memo);
+  // }
+
+  // time: O(n * log(n))
+  // space: O(n)
+  int lengthOfLIS(vector<int>& nums) {
+    vector<int> dp (1, INT_MIN);
+    for (auto num : nums) {
+      int pos = lower_bound(dp.begin(), dp.end(), num) - dp.begin();
+      if (pos == dp.size()) {
+        dp.push_back(num);
+      } else {
+        dp[pos] = num;
+      }
+    }
+    return dp.size() - 1;
+  }
+};
+
+int main() {
+  vector<int> nums;
+  for (int i = 1; i <= 2500; i++) nums.push_back(i);
+
+  Solution sol;
+  cout << sol.lengthOfLIS(nums);
+}
--- Leetcode/TODO/300_Longest_Increasing_Subsequence.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/longest-increasing-subsequence/
-
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        
-    }
-};
