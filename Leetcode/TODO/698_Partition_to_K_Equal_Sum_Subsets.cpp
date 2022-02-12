c18ab2a5abd155869b7b6d417f0c92ed4f23422a
brucerry <bruce.cheung111@gmail.com>
2022-02-13 03:22:28 +0800

    commit from vscode

---
+++ Leetcode/TODO/698_Partition_to_K_Equal_Sum_Subsets.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
+
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        
+    }
+};
---
+++ Leetcode/TODO/787_Cheapest_Flights_Within_K_Stops.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/cheapest-flights-within-k-stops/
+
+class Solution {
+public:
+  int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    
+  }
+};
