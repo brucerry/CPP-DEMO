79d1d073eee892f3ee1251fd98041432b1f06d75
brucerry <bruce.cheung111@gmail.com>
2022-02-16 20:16:28 +0800

    commit fom vscode

---
+++ Leetcode/221_Maximal_Square.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/maximal-square/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  // time: O(r * c)
+  // space: O(c)
+  int maximalSquare(vector<vector<char>>& matrix) {
+    int rows = matrix.size(), cols = matrix[0].size();
+    int dp[cols + 1];
+    memset(dp, 0, sizeof(dp));
+
+    int maxsqlen = 0, prev = 0;
+    for (int r = 1; r <= rows; r++) {
+      for (int c = 1; c <= cols; c++) {
+        int tmp = dp[c];
+        if (matrix[r-1][c-1] == '1') {
+          dp[c] = 1 + min({dp[c-1], prev, dp[c]});
+          maxsqlen = max(maxsqlen, dp[c]);
+        } else {
+          dp[c] = 0;
+        }
+        prev = tmp;
+      }
+    }
+
+    return maxsqlen * maxsqlen;
+  }
+};
--- Leetcode/TODO/221_Maximal_Square.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/maximal-square/
-
-class Solution {
-public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        
-    }
-};
