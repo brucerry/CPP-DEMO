2ad914772cdfe93cd315c714d9e32a5cf7750f0f
brucerry <bruce.cheung111@gmail.com>
2022-02-17 19:27:19 +0800

    commit from vscode

--- Leetcode/TODO/073_Set_Matrix_Zeroes.cpp
+++ Leetcode/TODO/073_Set_Matrix_Zeroes.cpp
@@ -1,8 +1,47 @@
 // https://leetcode.com/problems/set-matrix-zeroes/
 
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
+  // time: O(r * c)
+  // space: O(1)
   void setZeroes(vector<vector<int>>& matrix) {
-    
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+    bool setCol0 = false;
+
+    for (int r = 0; r < rows; r++) {
+      if (matrix[r][0] == 0) {
+        setCol0 = true;
+      }
+      for (int c = 1; c < cols; c++) {
+        if (matrix[r][c] == 0) {
+          matrix[r][0] = 0;
+          matrix[0][c] = 0;
+        }
+      }
+    }
+
+    for (int r = 1; r < rows; r++) {
+      for (int c = 1; c < cols; c++) {
+        if (matrix[r][0] == 0 || matrix[0][c] == 0) {
+          matrix[r][c] = 0;
+        }
+      }
+    }
+
+    if (matrix[0][0] == 0) {
+      for (int c = 1; c < cols; c++) {
+        matrix[0][c] = 0;
+      }
+    }
+
+    if (setCol0) {
+      for (int r = 0; r < rows; r++) {
+        matrix[r][0] = 0;
+      }
+    }
   }
 };
