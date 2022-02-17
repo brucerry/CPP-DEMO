95e742bc072bfc9ae4e4608bba9c49234962353e
brucerry <bruce.cheung111@gmail.com>
2022-02-18 02:55:57 +0800

    add TODO

---
+++ Leetcode/TODO/120_Triangle.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/triangle/
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+    }
+};
---
+++ Leetcode/TODO/746_Min_Cost_ClimbingStairs.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/min-cost-climbing-stairs/
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+    }
+};
