d6ad930f594a23c76feaac6d2b75b564143d3c27
brucerry <bruce.cheung111@gmail.com>
2022-02-21 04:04:42 +0800

    add todo

---
+++ Leetcode.com/TODO/141_Linked_List_Cycle.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+    }
+};
