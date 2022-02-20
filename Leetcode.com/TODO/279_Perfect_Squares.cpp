03fe734eb7ad4307787b33e912ece5f94b3c135a
brucerry <bruce.cheung111@gmail.com>
2022-02-21 04:11:25 +0800

    add todo

---
+++ Leetcode.com/TODO/279_Perfect_Squares.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/perfect-squares/
+
+class Solution {
+public:
+    int numSquares(int n) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/q1041_Robot_Bounded_In_Circle.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/robot-bounded-in-circle/
+
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        
+    }
+};
