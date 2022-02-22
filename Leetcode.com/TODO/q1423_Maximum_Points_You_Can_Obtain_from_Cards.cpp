777d0c0b3b677ebde0cdbbe49e74bf28541b5ac2
brucerry <bruce.cheung111@gmail.com>
2022-02-22 22:44:40 +0800

    add todo

---
+++ Leetcode.com/TODO/q1423_Maximum_Points_You_Can_Obtain_from_Cards.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/q1834_Single-Threaded_CPU.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/single-threaded-cpu/
+
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        
+    }
+};
