b1dc9a36d265f1a37b3366d3c41245d76e2007ef
brucerry <bruce.cheung111@gmail.com>
2022-02-28 23:23:14 +0800

    some works

---
+++ Stuff/Stack_Machine.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main() {
+  int n;
+  string cmd;
+
+  cin >> n;
+
+  while (n--) {
+    cin >> cmd;
+
+    if (cmd == "PUSH") {
+      
+    }
+
+    else if (cmd == "DUP") {
+      
+    }
+
+    else if (cmd == "UNION") {
+      
+    }
+
+    else if (cmd == "INTERSECT") {
+      
+    }
+
+    else {
+
+    }
+
+    if (stack.size()) {
+      cout << stack.top().size() << endl;
+    }
+  }
+}
