accdb25b75e5ed24edfc1590ccf5d0ba2d2dec10
brucerry <bruce.cheung111@gmail.com>
2022-03-01 22:34:12 +0800

    add todo

---
+++ Leetcode.com/TODO/034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+    }
+};
