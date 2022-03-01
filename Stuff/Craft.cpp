ae897a05a88d306c60a417c1c92dcb6b9b00113b
brucerry <bruce.cheung111@gmail.com>
2022-03-01 17:45:37 +0800

    completed

--- Stuff/Craft.cpp
+++ Stuff/Craft.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 // put the blocks stacked on the target block (slots[slot][index]) back to the top of their original slots
 void distribute(vector<vector<int>>& slots, vector<pair<int, int>>& location, int slot, int index) {
-  for (int i = index + 1; i < slots[slot].size(); i++) {
-    int block = slots[slot].back();
+  for (int i = slots[slot].size() - 1; i >= index + 1 ; i--) {
+    int block = slots[slot][i];
     slots[slot].pop_back();
     slots[block].push_back(block);
     location[block] = {block, slots[block].size() - 1};
@@ -21,7 +21,7 @@ int main() {
   cin >> n >> m;
 
   // init
-  vector<vector<int>> slots (n, vector<int>());
+  vector<vector<int>> slots (n);
   vector<pair<int, int>> location (n);
 
   for (int i = 0; i < n; i++) {
@@ -37,13 +37,10 @@ int main() {
     blockA--;
     blockB--;
     
-    auto [ slotA, indexA ] = location[blockA];
-    auto [ slotB, indexB ] = location[blockB];
+    auto& [ slotA, indexA ] = location[blockA];
+    auto& [ slotB, indexB ] = location[blockB];
 
-    if (slotA == slotB) {
-      // cout << "same slot" << endl;
-      continue;
-    }
+    if (slotA == slotB) continue;
 
     if (cmd1 == "move") {
       if (cmd2 == "onto") {
@@ -51,7 +48,7 @@ int main() {
         distribute(slots, location, slotB, indexB);
         slots[slotA].pop_back();
         slots[slotB].push_back(blockA);
-        location[blockA] = {slotB, slots[slotB].size() - 1};
+        location[blockA] = {slotB, indexB + 1};
       }
 
       else {
@@ -67,11 +64,18 @@ int main() {
         distribute(slots, location, slotB, indexB);
         slots[slotB].insert(slots[slotB].end(), slots[slotA].begin() + indexA, slots[slotA].end());
         slots[slotA].erase(slots[slotA].begin() + indexA, slots[slotA].end());
+        for (int i = indexB + 1; i < slots[slotB].size(); i++) {
+          location[slots[slotB][i]] = {slotB, i};
+        }
       }
 
       else {
+        int slotBsize = slots[slotB].size();
         slots[slotB].insert(slots[slotB].end(), slots[slotA].begin() + indexA, slots[slotA].end());
         slots[slotA].erase(slots[slotA].begin() + indexA, slots[slotA].end());
+        for (int i = slotBsize; i < slots[slotB].size(); i++) {
+          location[slots[slotB][i]] = {slotB, i};
+        }
       }
     }
   }
