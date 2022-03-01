3d165651050248638002bca9769c41434241ae38
brucerry <bruce.cheung111@gmail.com>
2022-03-01 19:38:51 +0800

    use emplace_back() & pass by pointers

--- Stuff/Craft.cpp
+++ Stuff/Craft.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 // put the blocks stacked on the target block (slots[slot][index]) back to the top of their original slots
-void distribute(vector<vector<int>>& slots, vector<pair<int, int>>& location, int slot, int index) {
-  for (int i = slots[slot].size() - 1; i >= index + 1 ; i--) {
-    int block = slots[slot][i];
-    slots[slot].pop_back();
-    slots[block].push_back(block);
+void distribute(vector<vector<int>>& slots, vector<pair<int, int>>& location, int* slot, int* index) {
+  for (int i = slots[*slot].size() - 1; i >= *index + 1 ; i--) {
+    int block = slots[*slot][i];
+    slots[*slot].pop_back();
+    slots[block].emplace_back(block);
     location[block] = {block, slots[block].size() - 1};
   }
 }
@@ -25,7 +25,7 @@ int main() {
   vector<pair<int, int>> location (n);
 
   for (int i = 0; i < n; i++) {
-    slots[i].push_back(i);
+    slots[i].emplace_back(i);
     location[i] = {i, 0};
   }
 
@@ -44,24 +44,24 @@ int main() {
 
     if (cmd1 == "move") {
       if (cmd2 == "onto") {
-        distribute(slots, location, slotA, indexA);
-        distribute(slots, location, slotB, indexB);
+        distribute(slots, location, &slotA, &indexA);
+        distribute(slots, location, &slotB, &indexB);
         slots[slotA].pop_back();
-        slots[slotB].push_back(blockA);
+        slots[slotB].emplace_back(blockA);
         location[blockA] = {slotB, indexB + 1};
       }
 
       else {
-        distribute(slots, location, slotA, indexA);
+        distribute(slots, location, &slotA, &indexA);
         slots[slotA].pop_back();
-        slots[slotB].push_back(blockA);
+        slots[slotB].emplace_back(blockA);
         location[blockA] = {slotB, slots[slotB].size() - 1};
       }
     }
 
     else {
       if (cmd2 == "onto") {
-        distribute(slots, location, slotB, indexB);
+        distribute(slots, location, &slotB, &indexB);
         slots[slotB].insert(slots[slotB].end(), slots[slotA].begin() + indexA, slots[slotA].end());
         slots[slotA].erase(slots[slotA].begin() + indexA, slots[slotA].end());
         for (int i = indexB + 1; i < slots[slotB].size(); i++) {
