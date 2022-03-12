b97b31da2d508c0be4b007e06a17a347b1be0358
brucerry <bruce.cheung111@gmail.com>
2022-03-13 03:59:06 +0800

    commit from vscode

--- Leetcode.com/Repository/269_Alien_Dictionary.cpp
+++ Leetcode.com/Repository/269_Alien_Dictionary.cpp
@@ -2,91 +2,70 @@
 
 // https://www.youtube.com/watch?v=6kTZYvNNyps
 
-#include <iostream>
 #include <vector>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
 using namespace std;
 
+// n = total number of characters in words
+// time: O(n)
+// space: O(n)
+
 class Solution {
 private:
-  int differentIndex(string s1, string s2) {
-    int i = 0;
-    while (i < s1.length()) {
-      if (s1[i] != s2[i]) return i;
-      i++;
+  bool hasCycle(unordered_map<char, unordered_set<char>> &graph, const char& node, string& result, unordered_set<char>& visiting, unordered_set<char>& visited) {
+    if (visiting.count(node)) return true;
+    if (visited.count(node)) return false;
+
+    visiting.insert(node);
+
+    for (auto& neighbors : graph[node]) {
+      if (hasCycle(graph, neighbors, result, visiting, visited)) return true;
     }
-    return -1; // error
+
+    visiting.erase(node);
+    visited.insert(node);
+
+    result += node;
+
+    return false;
   }
 
 public:
   string alienOrder(vector<string> words) {
     unordered_map<char, unordered_set<char>> graph;
-    unordered_set<char> allAlphabet;
-    for (auto word : words) {
-      for (auto ch : word) {
-        allAlphabet.insert(ch);
-        if (graph.count(ch) == 0) graph[ch] = {};
+
+    for (string& word : words) {
+      for (char& ch : word) {
+        graph[ch] = {};
       }
     }
 
-    for (int i = 0; i < words.size(); i++) {
-      for (int j = i + 1; j < words.size(); j++) {
-        if (words[j].find(words[i]) != string::npos) continue;
-        int idx = differentIndex(words[i], words[j]);
-        graph[words[i][idx]].insert(words[j][idx]);
-      }
-    }
+    for (int i = 0; i < words.size() - 1; i++) {
+      string& word1 = words[i];
+      string& word2 = words[i + 1];
+      int minLen = min(word1.length(), word2.length());
 
-    // print
-    // for (auto [ node, neighbors ] : graph) {
-    //   cout << node << ": ";
-    //   for (auto neighbor : neighbors) {
-    //     cout << neighbor << " ";
-    //   }
-    //   cout << endl;
-    // }
-    // print
+      if (word1.length() > word2.length() && word1.substr(0, minLen) == word2.substr(0, minLen)) return "";
 
-    unordered_map<char, int> numParents;
-    for (auto [ node, neighbors ] : graph) {
-      if (numParents.count(node) == 0) numParents[node] = 0;
-      for (auto neighbor : neighbors) {
-        numParents[neighbor]++;
-      }
-    }
-
-    vector<char> ready;
-    for (auto [ node, count ] : numParents) {
-      if (count == 0) ready.push_back(node);
-    }
-
-    string result;
-    unordered_set<char> visited;
-    while (ready.size()) {
-      char node = ready.back();
-      if (visited.count(node)) return "";
-      visited.insert(node);
-      result += node;
-      ready.pop_back();
-
-      for (auto neighbor : graph[node]) {
-        if (--numParents[neighbor] == 0) {
-          ready.push_back(neighbor);
+      for (int j = 0; j < minLen; j++) {
+        if (word1[j] != word2[j]) {
+          graph[word1[j]].insert(word2[j]);
+          break;
         }
       }
     }
 
-    return visited.size() == allAlphabet.size() ? result : "";
+    unordered_set<char> visiting;
+    unordered_set<char> visited;
+    string result;
+
+    for (auto& pair : graph) {
+      if (hasCycle(graph, pair.first, result, visiting, visited)) return "";
+    }
+
+    reverse(result.begin(), result.end());
+    return result;
   }
 };
-
-int main() {
-  Solution sol;
-  cout << sol.alienOrder({"wrt", "wrf", "er", "ett", "rftt"}) << endl; // "wertf"
-  cout << sol.alienOrder({"z", "x"}) << endl; // "zx"
-  cout << sol.alienOrder({"zy", "zx"}) << endl; // "yxz"
-  cout << sol.alienOrder({"a", "b", "c", "a"}) << endl; // ""
-  cout << sol.alienOrder({"ab", "bc", "cd", "a"}) << endl; // ""
-}
