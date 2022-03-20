a2dcaf5af2cd1aec65be3e5d03448b220ae65baa
brucerry <bruce.cheung111@gmail.com>
2022-03-21 04:07:53 +0800

    add todo

--- Leetcode.com/TODO/75 Questions/323_Number_of_Connected_Components_in_an_Undirected_Graph.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
-
-class Solution {
-public:
-  int countComponents(int n, vector<vector<int>>& edges) {
-    
-  }
-};
---
+++ Leetcode.com/TODO/Gerenal/547_Number_of_Provinces.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/number-of-provinces/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int findCircleNum(vector<vector<int>>& isConnected) {
+    
+  }
+};
