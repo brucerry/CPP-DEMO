a51023e1ec3efbecae1361544d1fcf5dae052d45
brucerry <bruce.cheung111@gmail.com>
2022-03-22 02:06:21 +0800

    add todo

---
+++ Leetcode.com/TODO/Gerenal/416_Partition_Equal_Subset_Sum.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/partition-equal-subset-sum/
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/Gerenal/605_Can_Place_Flowers.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/can-place-flowers/
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        
+    }
+};
