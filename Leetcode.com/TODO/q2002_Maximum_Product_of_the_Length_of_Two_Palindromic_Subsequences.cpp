418c5b0481daa2c965d1d010e98e28594b92e652
brucerry <bruce.cheung111@gmail.com>
2022-03-29 16:29:55 +0800

    add todo

---
+++ Leetcode.com/TODO/q2002_Maximum_Product_of_the_Length_of_Two_Palindromic_Subsequences.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/maximum-product-of-the-length-of-two-palindromic-subsequences/
+
+class Solution {
+public:
+    int maxProduct(string s) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/q2028_Find_Missing_Observations.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/find-missing-observations/
+
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        
+    }
+};
