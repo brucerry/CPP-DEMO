52d8ec1a793d0b6951d1303d94a5f4b5b3ae8027
brucerry <bruce.cheung111@gmail.com>
2022-04-02 20:10:28 +0800

    commit from vscode

---
+++ Leetcode.com/Repository/General/q2119_A_Number_After_a_Double_Reversal.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/a-number-after-a-double-reversal/
+
+// time: O(1)
+// space: O(1)
+
+class Solution {
+public:
+  bool isSameAfterReversals(int num) {
+    return num == 0 ? true : num % 10;
+  }
+};
