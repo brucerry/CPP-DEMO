b9b56139f9a3723abdea8616f8283ef2c9e9cfd6
brucerry <bruce.cheung111@gmail.com>
2022-04-02 22:13:59 +0800

    recap

--- Leetcode.com/Repository/75 Questions/191_Number_of_1_Bits.cpp
+++ Leetcode.com/Repository/75 Questions/191_Number_of_1_Bits.cpp
@@ -8,13 +8,13 @@
 class Solution {
 public:
   int hammingWeight(uint32_t n) {
-    int count = 0;
+    int ans = 0;
 
     while (n) {
       n &= (n - 1);
-      count++;
+      ans++;
     }
 
-    return count;
+    return ans;
   }
 };
--- Leetcode.com/Repository/75 Questions/198_House_Robber.cpp
+++ Leetcode.com/Repository/75 Questions/198_House_Robber.cpp
@@ -9,17 +9,16 @@ using namespace std;
 class Solution {
 public:
   int rob(vector<int>& nums) {
-    // index: 0, 1, 2, 3, ..., nums.size() - 1, rob1, rob2
+    // rob1, rob2, 0, 1, 2, 3, ..., nums.size() - 1
 
-    int rob1 = 0;
-    int rob2 = 0;
+    int rob1 = 0, rob2 = 0;
 
-    for (int i = nums.size() - 1; i >= 0; i--) {
-      int tmp = max(rob1, nums[i] + rob2);
-      rob2 = rob1;
-      rob1 = tmp;
+    for (int& num : nums) {
+      int tmp = max(num + rob1, rob2);
+      rob1 = rob2;
+      rob2 = tmp;
     }
 
-    return rob1;
+    return rob2;
   }
 };
