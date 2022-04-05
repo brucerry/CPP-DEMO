0f2ab1b81cad96f03b8cac663aff70160e4112ef
brucerry <bruce.cheung111@gmail.com>
2022-04-05 17:20:24 +0800

    slightly changes

--- Leetcode.com/Repository/General/002_Add_Two_Numbers.cpp
+++ Leetcode.com/Repository/General/002_Add_Two_Numbers.cpp
@@ -20,7 +20,7 @@ public:
     ListNode dummy = ListNode(), *cur = &dummy;
     int carry = 0;
     
-    while (l1 || l2) {
+    while (l1 || l2 || carry) {
       int val1 = l1 ? l1->val : 0;
       int val2 = l2 ? l2->val : 0;
       int sum = val1 + val2 + carry;
@@ -32,10 +32,6 @@ public:
       
       if (l1) l1 = l1->next;
       if (l2) l2 = l2->next;
-    }
-    
-    if (carry) {
-      cur->next = new ListNode(carry);
     }
     
     return dummy.next;
