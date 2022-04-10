6d20472b128619db4a89bc11010e338a683b6834
brucerry <bruce.cheung111@gmail.com>
2022-04-11 00:12:51 +0800

    add complexity analysis

--- Leetcode.com/Repository/General/543_Diameter_of_Binary_Tree.cpp
+++ Leetcode.com/Repository/General/543_Diameter_of_Binary_Tree.cpp
@@ -10,6 +10,9 @@ struct TreeNode {
   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// time: O(n)
+// space: O(height of tree)
+
 class Solution {
 private:
   int m_MaxDiameter = 0;
