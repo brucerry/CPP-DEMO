671957379625fb7758ea8efb9587cb1e9b444ee5
brucerry <bruce.cheung111@gmail.com>
2022-04-11 16:25:47 +0800

    commit from vscode

---
+++ Leetcode.com/Repository/General/110_Balanced_Binary_Tree.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/balanced-binary-tree/
+
+
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+  struct SubTreeState {
+    bool isBalanced;
+    int height;
+  };
+
+  SubTreeState helper(TreeNode* root) {
+    if (!root)
+      return { true, 0 };
+
+    SubTreeState leftState = helper(root->left);
+    
+    if (!leftState.isBalanced)
+      return { false, 0 };
+    
+    SubTreeState rightState = helper(root->right);
+    
+    if (!rightState.isBalanced)
+      return { false, 0 };
+
+    int height = 1 + max(leftState.height, rightState.height);
+    bool isBalanced = abs(leftState.height - rightState.height) < 2;
+
+    return { isBalanced, height };
+  }
+  
+public:
+  bool isBalanced(TreeNode* root) {
+    return helper(root).isBalanced;
+  }
+};
