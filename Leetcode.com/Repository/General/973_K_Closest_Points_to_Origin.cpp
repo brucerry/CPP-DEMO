9c517a198df26ed0fef18e645d6474215d724019
brucerry <bruce.cheung111@gmail.com>
2022-04-12 22:33:53 +0800

    recap

--- Leetcode.com/Repository/General/973_K_Closest_Points_to_Origin.cpp
+++ Leetcode.com/Repository/General/973_K_Closest_Points_to_Origin.cpp
@@ -5,61 +5,76 @@
 #include <queue>
 using namespace std;
 
-class _Solution {
+// time: O(n * log(k))
+// space: O(k)
+
+class Solution {
+private:
+  struct Point {
+    int dist, i;
+  };
+  
+  struct comp {
+    bool operator()(const Point& a, const Point& b) {
+      return a.dist < b.dist;
+    }
+  };
+  
+  int distance(const vector<int>& point) {
+    return point[0] * point[0] + point[1] * point[1];
+  }
+  
 public:
-  // time: O(n * log(k))
-  // space: O(k)
   vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
     // build a max heap with k capacity
-    priority_queue<pair<int, int>> maxHeap;
-
+    priority_queue<Point, vector<Point>, comp> maxHeap;
+    
     for (int i = 0; i < points.size(); i++) {
-      int distance = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+      Point p = { distance(points[i]), i };
       if (maxHeap.size() < k) {
-        maxHeap.push({distance, i});
+        maxHeap.emplace(p);
       }
-      else {
-        auto [ maxDistance, index ] = maxHeap.top();
-        if (distance < maxDistance) {
-          maxHeap.pop();
-          maxHeap.push({distance, i});
-        }
+      else if (p.dist < maxHeap.top().dist) {
+        maxHeap.pop();
+        maxHeap.emplace(p);
       }
     }
-
-    vector<vector<int>> result;
-
+    
+    vector<vector<int>> ans;
+    
     while (maxHeap.size()) {
-      auto [ distance, i ] = maxHeap.top();
+      const Point& p = maxHeap.top();
+      ans.push_back(points[p.i]);
       maxHeap.pop();
-      result.push_back({points[i][0], points[i][1]});
     }
-
-    return result;
+    
+    return ans;
   }
-
-
-  // time: O(n * log(n))
-  // space: O(n)
-  // vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-  //   priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
-
-  //   for (int i = 0; i < points.size(); i++) {
-  //     int x = points[i][0], y = points[i][1];
-  //     int distance = x * x + y * y;
-  //     minHeap.push({distance, i});
-  //   }
-
-  //   vector<vector<int>> result;
-
-  //   while (k--) {
-  //     int i = minHeap.top().index;
-  //     minHeap.pop();
-  //     int x = points[i][0];
-  //     int y = points[i][1];
-  //     result.push_back({x, y});
-  //   }
-
-  //   return result;
-  // }
 };
+
+// time: O(n * log(n))
+// space: O(n)
+// class _Solution {
+// public:
+//   vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+//     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
+
+//     for (int i = 0; i < points.size(); i++) {
+//       int x = points[i][0], y = points[i][1];
+//       int distance = x * x + y * y;
+//       minHeap.push({distance, i});
+//     }
+
+//     vector<vector<int>> result;
+
+//     while (k--) {
+//       int i = minHeap.top().index;
+//       minHeap.pop();
+//       int x = points[i][0];
+//       int y = points[i][1];
+//       result.push_back({x, y});
+//     }
+
+//     return result;
+//   }
+// };
