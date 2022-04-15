e53dbc589b7fc0c02afe0735683b503c76fa0c08
brucerry <bruce.cheung111@gmail.com>
2022-04-15 17:14:53 +0800

    commit from vscode

---
+++ Leetcode.com/Repository/General/684_Redundant_Connection.cpp
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/redundant-connection/
+
+#include <vector>
+#include <numeric>
+using namespace std;
+
+// time: O(n)
+// space: O(n)
+
+class Solution {
+public:
+  vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+    int n = edges.size();
+    
+    vector<int> parents (n + 1);
+    vector<int> ranks (n + 1, 1);
+    
+    iota(parents.begin(), parents.end(), 0);
+    
+    for (const auto& edge : edges) {
+      if (!unionNodes(parents, ranks, edge))
+        return edge;
+    }
+    
+    return {};
+  }
+  
+private:
+  int findParent(vector<int>& parents, int node) {
+    while (node != parents[node]) {
+      parents[node] = parents[parents[node]];
+      node = parents[node];
+    }
+    return node;
+  }
+  
+  bool unionNodes(vector<int>& parents, vector<int>& ranks, const vector<int>& edge) {
+    const int& node1 = edge[0];
+    const int& node2 = edge[1];
+    int parent1 = findParent(parents, node1);
+    int parent2 = findParent(parents, node2);
+  
+    if (parent1 == parent2)
+      return false;
+    
+    if (ranks[parent1] > ranks[parent2]) {
+      parents[parent2] = parent1;
+      ranks[parent1] += ranks[parent2];
+    }
+    else {
+      parents[parent1] = parent2;
+      ranks[parent2] += ranks[parent1];
+    }
+    
+    return true;
+  }
+};
--- Leetcode.com/TODO/684_Redundant_Connection.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/redundant-connection/
-
-class Solution {
-public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        
-    }
-};
