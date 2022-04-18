ba09e722e5b1517ce929702b5cf01f8681f64c94
brucerry <bruce.cheung111@gmail.com>
2022-04-18 18:47:12 +0800

    commit from vscode

---
+++ Leetcode.com/Repository/General/494_Target_Sum.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/target-sum/
+
+#include <vector>
+#include <numeric>
+using namespace std;
+
+// time: O(n * sum)
+// space: O(sum)
+
+class Solution {
+public:
+  int findTargetSumWays(vector<int>& nums, int target) {
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    
+    if (abs(target) > sum)
+      return 0;
+    
+    vector<int> dp ((sum << 1) + 1, 0); // range from -sum to sum
+    
+    // sum is the index of array half
+    dp[sum - nums[0]] += 1;
+    dp[sum + nums[0]] += 1;
+    
+    for (int i = 1; i < nums.size(); i++) {
+      vector<int> cur ((sum << 1) + 1, 0);
+      for (int s = -sum; s <= sum; s++) {
+        if (dp[sum + s]) {
+          cur[sum + s - nums[i]] += dp[sum + s];
+          cur[sum + s + nums[i]] += dp[sum + s];
+        }
+      }
+      dp = cur;
+    }
+    
+    return dp[sum + target];
+  }
+};
--- Leetcode.com/TODO/494_Target_Sum.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/target-sum/
-
-class Solution {
-public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        
-    }
-};
