5ad8c36bcf7537040fd02717fcdb61a78a95f029
brucerry <bruce.cheung111@gmail.com>
2022-04-19 19:43:57 +0800

    add stuff

---
+++ Stuff/024_Maximum_Score.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Item {
+  int score, mins;
+};
+
+int main() {
+  int n, m, s, t;
+
+  cin >> n >> m;
+
+  vector<Item> items (n);
+
+  for (int i = 0; i < n; i++) {
+    cin >> items[i].score >> items[i].mins;
+  }
+  
+  vector<int> dp (m + 1, 0);
+
+  for (int i = 0; i < n; i++) {
+    for (int j = m; j >= 0; j--) {
+      if (j < items[i].mins)
+        break;
+
+      dp[j] = max(dp[j], dp[j - items[i].mins] + items[i].score);
+    }
+  }
+
+  cout << dp[m];
+}
