1c1d7420674837082b06b9e476b301987a49b571
brucerry <bruce.cheung111@gmail.com>
2022-04-20 22:36:35 +0800

    add todo

---
+++ Leetcode.com/TODO/q1899_Merge_Triplets_to_Form_Target_Triplet.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/merge-triplets-to-form-target-triplet/
+
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        
+    }
+};
