58aaaa76de579aeb5639fe945a08b13f571106d3
brucerry <bruce.cheung111@gmail.com>
2022-04-21 23:34:47 +0800

    recap

--- Leetcode.com/Repository/General/202_Happy_Number.cpp
+++ Leetcode.com/Repository/General/202_Happy_Number.cpp
@@ -4,28 +4,27 @@
 // space: O(1)
 
 class Solution {
-private:
-  int sumOfSquare(int num) {
-    int sum = 0;
-    while (num) {
-      int d = num % 10;
-      sum += d * d;
-      num /= 10;
-    }
-    return sum;
-  }
-
 public:
   bool isHappy(int n) {
-    int slower = sumOfSquare(n);
-    int faster = sumOfSquare(sumOfSquare(n));
-
-    do {
-      if (faster == 1) return true;
-      slower = sumOfSquare(slower);
-      faster = sumOfSquare(sumOfSquare(faster));
-    } while (slower != faster);
-
-    return false;
+    int slow = sumOfSquare(n);
+    int fast = sumOfSquare(sumOfSquare(n));
+    
+    while (slow != fast) {
+      slow = sumOfSquare(slow);
+      fast = sumOfSquare(sumOfSquare(fast));
+    }
+    
+    return fast == 1;
+  }
+  
+private:
+  int sumOfSquare(int n) {
+    int sum = 0;
+    while (n) {
+      int d = n % 10;
+      sum += d * d;
+      n /= 10;
+    }
+    return sum;
   }
 };
