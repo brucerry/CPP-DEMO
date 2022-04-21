4e4b07c4e4f6dcb1f6ea86ef1b654afd923182c7
brucerry <bruce.cheung111@gmail.com>
2022-04-21 23:01:09 +0800

    recap

--- Leetcode.com/Repository/Blind 75/054_Spiral_Matrix.cpp
+++ Leetcode.com/Repository/Blind 75/054_Spiral_Matrix.cpp
@@ -9,20 +9,20 @@ using namespace std;
 class Solution {
 public:
   vector<int> spiralOrder(vector<vector<int>>& matrix) {
-    int left = 0;
-    int right = matrix[0].size() - 1;
-    int top = 0;
-    int bottom = matrix.size() - 1;
-
     vector<int> ans;
+    
+    int left = 0, right = matrix[0].size() - 1;
+    int top = 0, bottom = matrix.size() - 1;
 
     while (left <= right && top <= bottom) {
       // push top-most elements
-      for (int c = left; c <= right; c++) ans.push_back(matrix[top][c]);
+      for (int c = left; c <= right; c++)
+        ans.emplace_back(matrix[top][c]);
       top++;
 
       // push right-most elements
-      for (int r = top; r <= bottom; r++) ans.push_back(matrix[r][right]);
+      for (int r = top; r <= bottom; r++)
+        ans.emplace_back(matrix[r][right]);
       right--;
 
       // after top & right are modified
@@ -30,11 +30,13 @@ public:
       if (left > right || top > bottom) break;
 
       // push bottom-most elements
-      for (int c = right; c >= left; c--) ans.push_back(matrix[bottom][c]);
+      for (int c = right; c >= left; c--)
+        ans.emplace_back(matrix[bottom][c]);
       bottom--;
 
       // push left-most elements
-      for (int r = bottom; r >= top; r--) ans.push_back(matrix[r][left]);
+      for (int r = bottom; r >= top; r--)
+        ans.emplace_back(matrix[r][left]);
       left++;
     }
 
