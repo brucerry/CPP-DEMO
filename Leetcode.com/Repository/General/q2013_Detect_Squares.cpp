de5a047d161fa74b9bd1f8f74cb591b38cdd3b3a
brucerry <bruce.cheung111@gmail.com>
2022-04-22 16:25:53 +0800

    commit from vscode

---
+++ Leetcode.com/Repository/General/q2013_Detect_Squares.cpp
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/detect-squares/
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+// n = # of points(x, y)
+// time: O(1) for add(), O(n) for count()
+// space: O(n)
+
+class DetectSquares {
+public:
+  DetectSquares() {
+    
+  }
+  
+  void add(vector<int> point) {
+    int px = point[0];
+    int py = point[1];
+    pointCount[{ px, py }]++;
+  }
+  
+  int count(vector<int> point) {
+    int px = point[0];
+    int py = point[1];
+    
+    int ans = 0;
+    
+    for (const auto& [ p, count ] : pointCount) {
+      int x = p.first;
+      int y = p.second;
+      
+      if (x != px && y != py && abs(x - px) == abs(y - py) && pointCount.count({ x, py }) && pointCount.count({ px, y })) {
+        ans += count * pointCount[{ x, py }] * pointCount[{ px, y }];
+      }
+    }
+    
+    return ans;
+  }
+  
+private:
+  struct Hash {
+    int operator()(const pair<int, int>& p) const {
+      return hash<int>()(p.first) & hash<int>()(p.second);
+    }
+  };
+  
+  unordered_map<pair<int, int>, int, Hash> pointCount;
+};
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares* obj = new DetectSquares();
+ * obj->add(point);
+ * int param_2 = obj->count(point);
+ */
