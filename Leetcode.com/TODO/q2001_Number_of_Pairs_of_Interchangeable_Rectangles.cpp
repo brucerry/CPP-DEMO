00597866a3cf6ce9fbbd242fa85d5db12197a895
brucerry <bruce.cheung111@gmail.com>
2022-04-26 15:12:14 +0800

    add todo

---
+++ Leetcode.com/TODO/q2001_Number_of_Pairs_of_Interchangeable_Rectangles.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/number-of-pairs-of-interchangeable-rectangles/
+
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& rectangles) {
+        
+    }
+};
