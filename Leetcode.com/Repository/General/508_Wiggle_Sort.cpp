9689796e8bc4f7a4209d9c8cc9f0ef653c024620
brucerry <bruce.cheung111@gmail.com>
2022-04-26 22:00:17 +0800

    recap

--- Leetcode.com/Repository/General/508_Wiggle_Sort.cpp
+++ Leetcode.com/Repository/General/508_Wiggle_Sort.cpp
@@ -1,20 +1,17 @@
 // https://leetcode.com/problems/wiggle-sort/
 
-// https://www.youtube.com/watch?v=vGsyTE4s34w
-
 #include <vector>
 using namespace std;
 
 // time: O(n)
 // space: O(1)
+
 class Solution {
 public:
-  vector<int> wiggleSort(vector<int>& nums) {
+  void wiggleSort(vector<int> &nums) {
     for (int i = 1; i < nums.size(); i++) {
-      if ((i % 2 == 0 && nums[i-1] < nums[i]) || (i % 2 && nums[i-1] > nums[i])) {
+      if (((i & 1) and nums[i-1] > nums[i]) or (!(i & 1) and nums[i-1] < nums[i]))
         swap(nums[i-1], nums[i]);
-      }
     }
-    return nums;
   }
 };
