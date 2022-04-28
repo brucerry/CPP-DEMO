bb79d96c23d0dc6089a57a85962a16e67f98c186
brucerry <bruce.cheung111@gmail.com>
2022-04-28 23:06:34 +0800

    add todo

---
+++ Leetcode.com/TODO/895_Maximum_Frequency_Stack.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/maximum-frequency-stack/
+
+class FreqStack {
+public:
+    FreqStack() {
+        
+    }
+    
+    void push(int val) {
+        
+    }
+    
+    int pop() {
+        
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */
