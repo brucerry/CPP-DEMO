9e38164376d989a5ebb605490da664552a9f73a5
brucerry <bruce.cheung111@gmail.com>
2022-05-04 02:55:57 +0800

    recap

--- Leetcode.com/Repository/Blind 75/200_Number_of_Islands.cpp
+++ Leetcode.com/Repository/Blind 75/200_Number_of_Islands.cpp
@@ -7,31 +7,32 @@ using namespace std;
 // space: O(r * c)
 
 class Solution {
+public:
+  int numIslands(vector<vector<char>>& grid) {
+    int islandsCount = 0;
+    
+    for (int r = 0; r < grid.size(); r++) {
+      for (int c = 0; c < grid[0].size(); c++) {
+        if (exploreIsland(grid, r, c))
+          islandsCount++;
+      }
+    }
+    
+    return islandsCount;
+  }
+  
 private:
-  bool traverse(vector<vector<char>>& grid, int r, int c) {
-    if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || grid[r][c] == '0')
+  bool exploreIsland(vector<vector<char>>& grid, int r, int c) {
+    if (r < 0 or r >= grid.size() or c < 0 or c >= grid[0].size() or grid[r][c] == '0')
       return false;
     
     grid[r][c] = '0';
     
-    traverse(grid, r+1, c);
-    traverse(grid, r-1, c);
-    traverse(grid, r, c+1);
-    traverse(grid, r, c-1);
+    exploreIsland(grid, r+1, c);
+    exploreIsland(grid, r-1, c);
+    exploreIsland(grid, r, c+1);
+    exploreIsland(grid, r, c-1);
     
     return true;
-  }
-  
-public:
-  int numIslands(vector<vector<char>>& grid) {
-    int ans = 0;
-    
-    for (int r = 0; r < grid.size(); r++) {
-      for (int c = 0; c < grid[0].size(); c++) {
-        if (traverse(grid, r, c)) ans++;
-      }
-    }
-    
-    return ans;
   }
 };
