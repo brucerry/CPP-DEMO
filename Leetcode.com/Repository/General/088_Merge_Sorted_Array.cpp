e2b2349c50c7e387fdc02f87e670bbd38eeebf6a
brucerry <bruce.cheung111@gmail.com>
2022-05-09 18:47:55 +0800

    recap

--- Leetcode.com/Repository/General/088_Merge_Sorted_Array.cpp
+++ Leetcode.com/Repository/General/088_Merge_Sorted_Array.cpp
@@ -10,16 +10,14 @@ using namespace std;
 class Solution {
 public:
   void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    int replaceIndex = m + n - 1;
-    m--;
-    n--;
-
-    while (n >= 0) {
-      if (m < 0 || nums2[n] >= nums1[m]) {
-        nums1[replaceIndex--] = nums2[n--];
+    int i = m - 1, j = n - 1, k = m + n - 1;
+    
+    while (j >= 0) {
+      if (i < 0 or nums1[i] < nums2[j]) {
+        nums1[k--] = nums2[j--];
       }
       else {
-        nums1[replaceIndex--] = nums1[m--];
+        nums1[k--] = nums1[i--];
       }
     }
   }
