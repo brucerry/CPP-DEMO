bb9bc5902fa939bc36b2e8704b66cb62e4cb079e
brucerry <bruce.cheung111@gmail.com>
2022-05-14 17:30:38 +0800

    recap

--- Leetcode.com/Repository/Blind 75/141_Linked_List_Cycle.cpp
+++ Leetcode.com/Repository/Blind 75/141_Linked_List_Cycle.cpp
@@ -1,10 +1,9 @@
 // https://leetcode.com/problems/linked-list-cycle/
 
-
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(nullptr) {}
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(nullptr) {}
 };
 
 // time: O(n)
@@ -15,7 +14,7 @@ public:
   bool hasCycle(ListNode *head) {
     ListNode* slow = head, *fast = head;
     
-    while (fast && fast->next) {
+    while (fast and fast->next) {
       slow = slow->next;
       fast = fast->next->next;
       if (slow == fast)
