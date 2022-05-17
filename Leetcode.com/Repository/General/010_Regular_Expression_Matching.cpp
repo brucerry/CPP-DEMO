f32396d69b0754fa43ea4e8112f3864d1a56d319
brucerry <bruce.cheung111@gmail.com>
2022-05-17 22:42:16 +0800

    recap + optimize space

--- Leetcode.com/Repository/General/010_Regular_Expression_Matching.cpp
+++ Leetcode.com/Repository/General/010_Regular_Expression_Matching.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 // time: O(s * p)
 // space: O(s * p)
-
 class TopDownMemo {
 public:
-  bool isMatch(string s, string p) {
-    vector<vector<char>> memo (s.length() + 1, vector<char>(p.length() + 1, -1));
+  bool isMatch(string& s, string& p) {
+    int m = s.length();
+    int n = p.length();
+    
+    vector<vector<char>> memo (m + 1, vector<char>(n + 1, -1));
     return solve(s, p, 0, 0, memo);
   }
   
@@ -22,32 +24,67 @@ private:
     if (memo[i][j] != -1)
       return memo[i][j];
     
-    bool match = i < s.length() && (s[i] == p[j] || p[j] == '.');
+    bool match = i < s.length() and (s[i] == p[j] or p[j] == '.');
     
-    if (j + 1 < p.length() && p[j + 1] == '*')
-      return memo[i][j] = solve(s, p, i, j + 2, memo) || (match && solve(s, p, i + 1, j, memo));
-  
-    return memo[i][j] = match && solve(s, p, i + 1, j + 1, memo);
+    if (j + 1 < p.length() and p[j+1] == '*')
+      return memo[i][j] = (match and solve(s, p, i+1, j, memo)) or solve(s, p, i, j+2, memo);
+    
+    return memo[i][j] = match and solve(s, p, i+1, j+1, memo);
   }
 };
 
-class BottomUpDP {
+// time: O(s * p)
+// space: O(s * p)
+class BottomUpDP_2D {
 public:
-  bool isMatch(string s, string p) {
-    vector<vector<char>> dp (s.length() + 1, vector<char>(p.length() + 1, 0));
+  bool isMatch(string& s, string& p) {
+    int m = s.length();
+    int n = p.length();
+    
+    vector<vector<char>> dp (m + 1, vector<char>(n + 1));
     dp.back().back() = 1;
     
-    for (int i = s.length(); i >= 0; i--) {
-      for (int j = p.length() - 1; j >= 0; j--) {
-        bool match = i < s.length() && (s[i] == p[j] || p[j] == '.');
-        
-        if (j + 1 < p.length() && p[j + 1] == '*')
-          dp[i][j] = (match && dp[i + 1][j]) || dp[i][j + 2];
+    for (int i = m; i >= 0; i--) {
+      for (int j = n - 1; j >= 0; j--) {
+        bool match = i < m and (s[i] == p[j] or p[j] == '.');
+        if (j + 1 < n and p[j+1] == '*')
+          dp[i][j] = (match and dp[i+1][j]) or dp[i][j+2];
         else
-          dp[i][j] = match && dp[i + 1][j + 1];
+          dp[i][j] = match and dp[i+1][j+1];
       }
     }
     
     return dp[0][0];
+  }
+};
+
+// time: O(s * p)
+// space: O(p)
+class BottomUpDP_1D {
+public:
+  bool isMatch(string& s, string& p) {
+    int m = s.length();
+    int n = p.length();
+    
+    vector<char> dp (n + 1);
+    
+    for (int i = m; i >= 0; i--) {
+      int prev = i == m - 1 ? 1 : 0;
+      dp.back() = i == m ? 1 : 0;
+      
+      for (int j = n - 1; j >= 0; j--) {
+        int tmp = dp[j];
+        
+        bool match = i < m and (s[i] == p[j] or p[j] == '.');
+        if (j + 1 < n and p[j+1] == '*')
+          dp[j] = (match and dp[j]) or dp[j+2];
+        else
+          dp[j] = match and prev;
+        
+        prev = tmp;
+      }
+    }
+    
+    return dp[0];
   }
 };
