6cbd66d6a82b9ea479a27b8a45c69e4e0e57a261
brucerry <bruce.cheung111@gmail.com>
2022-06-03 20:44:55 +0800

    recap

--- Leetcode.com/Repository/General/684_Redundant_Connection.cpp
+++ Leetcode.com/Repository/General/684_Redundant_Connection.cpp
@@ -13,12 +13,13 @@ public:
     
     vector<int> parents (n + 1);
     vector<int> ranks (n + 1, 1);
-    for (int node = 1; node <= n; node++) {
+    for (int node = 1; node <= n; node++)
       parents[node] = node;
-    }
     
     for (const auto& edge : edges) {
-      if (!unionNodes(parents, ranks, edge))
+      int node1 = edge[0];
+      int node2 = edge[1];
+      if (!unionNodes(parents, ranks, node1, node2))
         return edge;
     }
     
@@ -26,32 +27,30 @@ public:
   }
   
 private:
-  int findParent(vector<int>& parents, int node) {
+  bool unionNodes(vector<int>& parents, vector<int>& ranks, int node1, int node2) {
+    int parent1 = findParent(parents, node1);
+    int parent2 = findParent(parents, node2);
+    
+    if (parent1 == parent2)
+      return false;
+    
+    if (ranks[parent1] > ranks[parent2]) {
+      ranks[parent1] += ranks[parent2];
+      parents[parent2] = parent1;
+    }
+    else {
+      ranks[parent2] += ranks[parent1];
+      parents[parent1] = parent2;
+    }
+    
+    return true;
+  }
+  
+  int findParent (vector<int>& parents, int node) {
     while (node != parents[node]) {
       parents[node] = parents[parents[node]];
       node = parents[node];
     }
     return node;
-  }
-  
-  bool unionNodes(vector<int>& parents, vector<int>& ranks, const vector<int>& edge) {
-    const int& node1 = edge[0];
-    const int& node2 = edge[1];
-    int parent1 = findParent(parents, node1);
-    int parent2 = findParent(parents, node2);
-  
-    if (parent1 == parent2)
-      return false;
-    
-    if (ranks[parent1] > ranks[parent2]) {
-      parents[parent2] = parent1;
-      ranks[parent1] += ranks[parent2];
-    }
-    else {
-      parents[parent1] = parent2;
-      ranks[parent2] += ranks[parent1];
-    }
-    
-    return true;
   }
 };
