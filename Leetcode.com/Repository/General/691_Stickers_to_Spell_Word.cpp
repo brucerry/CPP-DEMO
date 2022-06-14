c9cf02c07c409167dae7c97098258938a7f2a50f
brucerry <bruce.cheung111@gmail.com>
2022-06-14 18:56:19 +0800

    recap

--- Leetcode.com/Repository/General/691_Stickers_to_Spell_Word.cpp
+++ Leetcode.com/Repository/General/691_Stickers_to_Spell_Word.cpp
@@ -2,57 +2,61 @@
 
 #include <vector>
 #include <string>
+#include <array>
 #include <unordered_map>
 using namespace std;
 
-// s = size of stickers
+// n = size of stickers
+// s = len of each sticker
 // t = len of target
-// time: O(2^t * s)
-// space: O(t)
+// time: O(n * s + 2^t * n)
+// space: O(n + 2^t)
 
 class Solution {
 public:
   int minStickers(vector<string>& stickers, string& target) {
     int n = stickers.size();
     
-    vector<vector<int>> stickersCharCount (n, vector<int>(26));
-    for (int sticker = 0; sticker < n; sticker++) {
-      for (const char& c : stickers[sticker])
-        stickersCharCount[sticker][c - 'a']++;
+    vector<vector<int>> stickerCharCount (n, vector<int>(26));
+    for (int i = 0; i < n; i++) {
+      for (const char& c : stickers[i]) {
+        stickerCharCount[i][c - 'a']++;
+      }
     }
     
-    unordered_map<string, int> memo; // remain target, used stickers count
-    return solve(target, stickersCharCount, memo);
+    unordered_map<string, int> memo; // remain target, min # of stickers
+    return solve(target, stickerCharCount, memo);
   }
   
 private:
-  int solve(string& target, vector<vector<int>>& stickersCharCount, unordered_map<string, int>& memo) {
+  int solve(string& target, vector<vector<int>>& stickerCharCount, unordered_map<string, int>& memo) {
     if (target.length() == 0)
       return 0;
     
     if (memo.count(target))
       return memo[target];
     
-    vector<int> targetCharCount (26);
-    for (const char& c : target)
+    array<int, 26> targetCharCount { 0 };
+    for (const char& c : target) {
       targetCharCount[c - 'a']++;
+    }
     
     int result = INT_MAX;
-    for (const auto& charCount : stickersCharCount) {
-      if (charCount[target[0] - 'a'] == 0)
+    for (const auto& scc : stickerCharCount) {
+      if (scc[target[0] - 'a'] == 0)
         continue;
       
       string remainTarget;
       for (int i = 0; i < 26; i++) {
-        if (targetCharCount[i] > charCount[i])
-          remainTarget += string(targetCharCount[i] - charCount[i], i + 'a');
+        if (targetCharCount[i] > scc[i])
+          remainTarget += string(targetCharCount[i] - scc[i], i + 'a');
       }
       
-      int usedStickersCount = solve(remainTarget, stickersCharCount, memo);
-      if (usedStickersCount != -1)
-        result = min(result, usedStickersCount + 1);
+      int usedCount = solve(remainTarget, stickerCharCount, memo);
+      if (usedCount != -1)
+        result = min(result, 1 + usedCount);
     }
     
-    return memo[target] = result == INT_MAX ? -1 : result; 
+    return memo[target] = result == INT_MAX ? -1 : result;
   }
 };
