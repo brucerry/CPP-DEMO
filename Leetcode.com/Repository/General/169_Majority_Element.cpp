590657e39de0928585dd9a0cb8f0f438ace6c408
brucerry <bruce.cheung111@gmail.com>
2022-06-17 01:48:29 +0800

    recap

--- Leetcode.com/Repository/General/169_Majority_Element.cpp
+++ Leetcode.com/Repository/General/169_Majority_Element.cpp
@@ -10,13 +10,11 @@ class Solution {
 public:
   int majorityElement(vector<int>& nums) {
     int count = 0, result = 0;
-    
     for (const int& num : nums) {
       if (count == 0)
         result = num;
       count += num == result ? 1 : -1;
     }
-    
     return result;
   }
 };
