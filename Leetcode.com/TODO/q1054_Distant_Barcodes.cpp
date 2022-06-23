d3a8209e678bd16ed6aa99c9fc82367898d9f5c2
brucerry <bruce.cheung111@gmail.com>
2022-06-23 19:06:03 +0800

    add todo

---
+++ Leetcode.com/TODO/q1054_Distant_Barcodes.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/distant-barcodes/
+
+class Solution {
+public:
+    vector<int> rearrangeBarcodes(vector<int>& barcodes) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/q1186_Maximum_Subarray_Sum_with_One_Deletion.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/maximum-subarray-sum-with-one-deletion/
+
+class Solution {
+public:
+    int maximumSum(vector<int>& arr) {
+        
+    }
+};
