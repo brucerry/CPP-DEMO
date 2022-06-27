289516d443b07b8232ccf198f300d255b72a74a0
brucerry <bruce.cheung111@gmail.com>
2022-06-27 17:53:29 +0800

    recap

--- Leetcode.com/Repository/Blind 75/139_Word_Break.cpp
+++ Leetcode.com/Repository/Blind 75/139_Word_Break.cpp
@@ -5,22 +5,23 @@
 using namespace std;
 
 // s = len of s
-// w = size of wordDict
-// time: O(s * w)
+// n = size of wordDict
+// time: O(s * n)
 // space: O(s)
 
 class Solution {
 public:
   bool wordBreak(string& s, vector<string>& wordDict) {
-    vector<char> dp (s.length() + 1);
+    int n = s.length();
+    vector<char> dp (n + 1);
     dp.back() = 1;
     
-    for (int i = s.length() - 1; i >= 0; i--) {
+    for (int i = n - 1; i >= 0; i--) {
       for (const string& word : wordDict) {
-        if (i == s.find(word, i) and dp[i + word.length()]) {
-          dp[i] = 1;
+        if (i == s.find(word, i))
+          dp[i] |= dp[i + word.length()];
+        if (dp[i])
           break;
-        }
       }
     }
     
