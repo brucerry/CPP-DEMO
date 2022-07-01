6ad7851660a75300f216498ae0ebd4e9aa2dad2c
brucerry <bruce.cheung111@gmail.com>
2022-07-01 23:43:52 +0800

    add todo

---
+++ Leetcode.com/TODO/q2279_Maximum_Bags_With_Full_Capacity_of_Rocks.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/
+
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        
+    }
+};
