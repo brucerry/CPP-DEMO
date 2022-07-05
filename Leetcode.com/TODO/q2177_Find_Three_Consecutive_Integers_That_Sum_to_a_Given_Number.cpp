bfb1eb33f1c08c942cc4855a00256fec56a5b9a5
brucerry <bruce.cheung111@gmail.com>
2022-07-05 17:07:24 +0800

    add todo

---
+++ Leetcode.com/TODO/q2177_Find_Three_Consecutive_Integers_That_Sum_to_a_Given_Number.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/find-three-consecutive-integers-that-sum-to-a-given-number/
+
+class Solution {
+public:
+    vector<long long> sumOfThree(long long num) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/q2274_Maximum_Consecutive_Floors_Without_Special_Floors.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/maximum-consecutive-floors-without-special-floors/
+
+class Solution {
+public:
+    int maxConsecutive(int bottom, int top, vector<int>& special) {
+        
+    }
+};
