7b038bbe57b4843ef015337ffb5cd942bbf65eb0
brucerry <bruce.cheung111@gmail.com>
2022-07-07 23:16:03 +0800

    add todo

--- Leetcode.com/TODO/q2177_Find_Three_Consecutive_Integers_That_Sum_to_a_Given_Number.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/find-three-consecutive-integers-that-sum-to-a-given-number/
-
-class Solution {
-public:
-    vector<long long> sumOfThree(long long num) {
-        
-    }
-};
---
+++ Leetcode.com/TODO/q2240_Number_of_Ways_to_Buy_Pens_and_Pencils.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/number-of-ways-to-buy-pens-and-pencils/
+
+class Solution {
+public:
+    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        
+    }
+};
