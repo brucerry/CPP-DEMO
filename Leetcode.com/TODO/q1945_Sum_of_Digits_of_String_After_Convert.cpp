1b4c415ed91f24bcfaea80ac50472bd18c431def
brucerry <bruce.cheung111@gmail.com>
2022-07-12 22:55:20 +0800

    add todo

---
+++ Leetcode.com/TODO/q1945_Sum_of_Digits_of_String_After_Convert.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
+
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/q2310_Sum_of_Numbers_With_Units_Digit_K.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/sum-of-numbers-with-units-digit-k/
+
+class Solution {
+public:
+    int minimumNumbers(int num, int k) {
+        
+    }
+};
