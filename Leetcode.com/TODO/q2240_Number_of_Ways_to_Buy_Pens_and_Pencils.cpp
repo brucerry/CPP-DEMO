4fff5592df08cbdffff92c6e315cff65ebfdbd2b
brucerry <bruce.cheung111@gmail.com>
2022-07-12 22:55:13 +0800

    done

--- Leetcode.com/TODO/q2240_Number_of_Ways_to_Buy_Pens_and_Pencils.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/number-of-ways-to-buy-pens-and-pencils/
-
-class Solution {
-public:
-    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
-        
-    }
-};
--- Leetcode.com/TODO/q2274_Maximum_Consecutive_Floors_Without_Special_Floors.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/maximum-consecutive-floors-without-special-floors/
-
-class Solution {
-public:
-    int maxConsecutive(int bottom, int top, vector<int>& special) {
-        
-    }
-};
