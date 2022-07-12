a1222d50b21524a1857445ed74eeed661c978306
brucerry <bruce.cheung111@gmail.com>
2022-07-12 15:25:52 +0800

    add todo

---
+++ Leetcode.com/TODO/q1691_Maximum_Height_by_Stacking_Cuboids.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/maximum-height-by-stacking-cuboids/
+
+class Solution {
+public:
+    int maxHeight(vector<vector<int>>& cuboids) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/q1703_Minimum_Adjacent_Swaps_for_K_Consecutive_Ones.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/minimum-adjacent-swaps-for-k-consecutive-ones/
+
+class Solution {
+public:
+    int minMoves(vector<int>& nums, int k) {
+        
+    }
+};
