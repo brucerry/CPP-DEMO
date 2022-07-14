ff7afc6f629bccd2a084e4fba96600e668ba59d0
brucerry <bruce.cheung111@gmail.com>
2022-07-14 17:28:17 +0800

    recap

--- Leetcode.com/Repository/Blind 75/105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
+++ Leetcode.com/Repository/Blind 75/105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
 #include <vector>
+#include <algorithm>
+#include <unordered_map>
 using namespace std;
 
 struct TreeNode {
@@ -13,9 +15,66 @@ struct TreeNode {
 };
 
 // time: O(n)
-// space: O(n)
+// space: O(height of tree)
+class TwoPointers {
+public:
+  TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    int pre = 0, in = 0;
+    return solve(preorder, inorder, pre, in, INT_MIN);
+  }
+  
+private:
+  TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int& pre, int& in, int stop) {
+    if (pre == preorder.size())
+      return nullptr;
+    
+    if (inorder[in] == stop) {
+      in++;
+      return nullptr;
+    }
+    
+    int rootVal = preorder[pre++];
+    TreeNode* leftNode = solve(preorder, inorder, pre, in, rootVal);
+    TreeNode* rightNode = solve(preorder, inorder, pre, in, stop);
+    
+    return new TreeNode(rootVal, leftNode, rightNode);
+  }
+};
 
-class Solution {
+// time: O(n)
+// space: O(n)
+class RuntimeOptimized {
+public:
+  TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    int n = preorder.size();
+    
+    unordered_map<int, int> nodeIndex; // nodeVal, inorder index
+    for (int i = 0; i < n; i++) {
+      int val = inorder[i];
+      nodeIndex[val] = i;
+    }
+    
+    return solve(preorder, inorder, 0, n - 1, 0, n - 1, nodeIndex);
+  }
+  
+private:
+  TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int preStart, int preEnd, int inStart, int inEnd, unordered_map<int, int>& nodeIndex) {
+    if (preStart > preEnd)
+      return nullptr;
+    
+    int rootVal = preorder[preStart];
+    int rootIndex = nodeIndex[rootVal];
+    int leftSize = rootIndex - inStart;
+    
+    TreeNode* leftNode = solve(preorder, inorder, preStart + 1, preStart + leftSize, inStart, rootIndex - 1, nodeIndex);
+    TreeNode* rightNode = solve(preorder, inorder, preStart + leftSize + 1, preEnd, rootIndex + 1, inEnd, nodeIndex);
+    return new TreeNode(rootVal, leftNode, rightNode);
+  }
+};
+
+// time: O(n^2)
+// space: O(height of tree)
+class SearchValueEveryTime {
 public:
   TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
     return solve(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
