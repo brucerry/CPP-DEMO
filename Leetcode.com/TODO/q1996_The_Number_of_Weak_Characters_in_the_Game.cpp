c3c93786eed6593170e4dd639909e128fdb7aff7
brucerry <bruce.cheung111@gmail.com>
2022-07-20 00:36:14 +0800

    done

--- Leetcode.com/TODO/q1996_The_Number_of_Weak_Characters_in_the_Game.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/
-
-class Solution {
-public:
-    int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        
-    }
-};
