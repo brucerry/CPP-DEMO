eca08fb085577ba506f1fe8550284405d3abffe5
brucerry <bruce.cheung111@gmail.com>
2022-07-20 22:47:08 +0800

    done

--- Leetcode.com/TODO/685_Redundant_Connection_II.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/redundant-connection-ii/
-
-class Solution {
-public:
-    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
-        
-    }
-};
--- Leetcode.com/TODO/q2078_Two_Furthest_Houses_With_Different_Colors.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/two-furthest-houses-with-different-colors/
-
-class Solution {
-public:
-    int maxDistance(vector<int>& colors) {
-        
-    }
-};
