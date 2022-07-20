3a5bf3634338da11116508c03c4bb8d64a09cec2
brucerry <bruce.cheung111@gmail.com>
2022-07-20 22:47:06 +0800

    add todo

---
+++ Leetcode.com/TODO/529_Minesweeper.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/minesweeper/
+
+class Solution {
+public:
+    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/797_All_Paths_From_Source_to_Target.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/all-paths-from-source-to-target/
+
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        
+    }
+};
