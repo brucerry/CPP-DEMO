eeb6b3060a8b95b660b50ad324a292b5bd7ded75
brucerry <bruce.cheung111@gmail.com>
2022-07-25 18:48:14 +0800

    add todo

---
+++ Leetcode.com/TODO/q2055_Plates_Between_Candles.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/plates-between-candles/
+
+class Solution {
+public:
+    vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/q2089_Find_Target_Indices_After_Sorting_Array.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/find-target-indices-after-sorting-array/
+
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        
+    }
+};
