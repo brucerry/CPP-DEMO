a87a197c4ed574e1aa076802853c3d08b8e9a7a3
brucerry <bruce.cheung111@gmail.com>
2022-07-27 17:50:07 +0800

    add todo

---
+++ Leetcode.com/TODO/q1177_Can_Make_Palindrome_from_Substring.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/can-make-palindrome-from-substring/
+
+class Solution {
+public:
+    vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/q1331_Rank_Transform_of_an_Array.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/rank-transform-of-an-array/
+
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        
+    }
+};
