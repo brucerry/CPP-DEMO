ed41321a5f8be8e7a351a3fa96c8b6b9e57a17c8
brucerry <bruce.cheung111@gmail.com>
2022-07-28 19:42:25 +0800

    done

--- Leetcode.com/TODO/q1177_Can_Make_Palindrome_from_Substring.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/can-make-palindrome-from-substring/
-
-class Solution {
-public:
-    vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
-        
-    }
-};
--- Leetcode.com/TODO/q1944_Number_of_Visible_People_in_a_Queue.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/number-of-visible-people-in-a-queue/
-
-class Solution {
-public:
-    vector<int> canSeePersonsCount(vector<int>& heights) {
-        
-    }
-};
