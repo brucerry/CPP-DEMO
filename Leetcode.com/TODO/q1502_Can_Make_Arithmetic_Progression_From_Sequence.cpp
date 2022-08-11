94bbc14e1cd717c64e9c06824bb29377aa74ada8
brucerry <bruce.cheung111@gmail.com>
2022-08-11 23:31:42 +0800

    add todo

---
+++ Leetcode.com/TODO/q1502_Can_Make_Arithmetic_Progression_From_Sequence.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        
+    }
+};
