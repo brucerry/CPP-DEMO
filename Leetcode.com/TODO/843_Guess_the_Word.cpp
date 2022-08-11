14044e18935b2f1a94534bc741cd3da893977113
brucerry <bruce.cheung111@gmail.com>
2022-08-11 23:33:39 +0800

    add todo

---
+++ Leetcode.com/TODO/843_Guess_the_Word.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/guess-the-word/
+
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Master {
+ *   public:
+ *     int guess(string word);
+ * };
+ */
+class Solution {
+public:
+    void findSecretWord(vector<string>& words, Master& master) {
+        
+    }
+};
