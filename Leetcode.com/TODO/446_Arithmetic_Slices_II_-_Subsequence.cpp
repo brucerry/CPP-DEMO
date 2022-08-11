793bbd2a22bc1a8ef1a50ead2cf0dd0427620598
brucerry <bruce.cheung111@gmail.com>
2022-08-11 23:31:37 +0800

    done

--- Leetcode.com/TODO/446_Arithmetic_Slices_II_-_Subsequence.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/arithmetic-slices-ii-subsequence/
-
-class Solution {
-public:
-    int numberOfArithmeticSlices(vector<int>& nums) {
-        
-    }
-};
--- Leetcode.com/TODO/q1630_Arithmetic_Subarrays.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/arithmetic-subarrays/
-
-class Solution {
-public:
-    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
-        
-    }
-};
