8ee267c981cae9687ed1d208154dee4bbeddf98f
brucerry <bruce.cheung111@gmail.com>
2022-08-19 15:53:48 +0800

    add todo

---
+++ Leetcode.com/TODO/142_Linked_List_Cycle_II.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/linked-list-cycle-ii/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/813_Largest_Sum_of_Averages.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/largest-sum-of-averages/
+
+class Solution {
+public:
+    double largestSumOfAverages(vector<int>& nums, int k) {
+        
+    }
+};
