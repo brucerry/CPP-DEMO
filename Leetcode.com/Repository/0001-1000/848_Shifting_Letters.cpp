e1c06d68730140d3c1a655b100e888d91749d2a1
brucerry <bruce.cheung111@gmail.com>
2022-08-21 22:11:30 +0800

    commit from vscode

---
+++ Leetcode.com/Repository/0001-1000/848_Shifting_Letters.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/shifting-letters/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+// time: O(n)
+// space: O(1)
+
+class Solution {
+public:
+  string shiftingLetters(string& s, vector<int>& shifts) {
+    int n = s.length();
+    
+    for (int i = n - 2; i >= 0; i--) {
+      shifts[i] = (shifts[i] + shifts[i+1]) % 26;
+    }
+    
+    for (int i = 0; i < n; i++) {
+      s[i] = (s[i] - 'a' + shifts[i]) % 26 + 'a';
+    }
+    
+    return s;
+  }
+};
