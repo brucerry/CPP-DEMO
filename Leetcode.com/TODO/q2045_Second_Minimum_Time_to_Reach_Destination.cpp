66e9d17ec337a3d2f4a2d5d1d09dc596c9685b45
brucerry <bruce.cheung111@gmail.com>
2022-08-26 17:45:16 +0800

    done

--- Leetcode.com/TODO/q2045_Second_Minimum_Time_to_Reach_Destination.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/second-minimum-time-to-reach-destination/
-
-class Solution {
-public:
-    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
-        
-    }
-};
