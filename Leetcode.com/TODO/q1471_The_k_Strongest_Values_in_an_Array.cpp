94e0b4e5cadad89e399bae932fe7a638629aee23
brucerry <bruce.cheung111@gmail.com>
2022-09-02 16:18:54 +0800

    done

--- Leetcode.com/TODO/q1471_The_k_Strongest_Values_in_an_Array.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/the-k-strongest-values-in-an-array/
-
-class Solution {
-public:
-    vector<int> getStrongest(vector<int>& arr, int k) {
-        
-    }
-};
--- Leetcode.com/TODO/q2087_Minimum_Cost_Homecoming_of_a_Robot_in_a_Grid.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/minimum-cost-homecoming-of-a-robot-in-a-grid/
-
-class Solution {
-public:
-    int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
-        
-    }
-};
