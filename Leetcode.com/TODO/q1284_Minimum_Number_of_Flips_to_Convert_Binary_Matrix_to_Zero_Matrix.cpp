0f7c5388576b3907af3a2a608d12786cd2998192
brucerry <bruce.cheung111@gmail.com>
2022-09-03 15:30:13 +0800

    add todo

---
+++ Leetcode.com/TODO/q1284_Minimum_Number_of_Flips_to_Convert_Binary_Matrix_to_Zero_Matrix.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/
+
+class Solution {
+public:
+    int minFlips(vector<vector<int>>& mat) {
+        
+    }
+};
---
+++ Leetcode.com/TODO/q2048_Next_Greater_Numerically_Balanced_Number.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/next-greater-numerically-balanced-number/
+
+class Solution {
+public:
+    int nextBeautifulNumber(int n) {
+        
+    }
+};
