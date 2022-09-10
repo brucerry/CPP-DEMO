493b5608bb26118cd15074cc9f54a3a111e1e39a
brucerry <bruce.cheung111@gmail.com>
2022-09-10 18:26:05 +0800

    done

--- Leetcode.com/TODO/q1458_Max_Dot_Product_of_Two_Subsequences.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/max-dot-product-of-two-subsequences/
-
-class Solution {
-public:
-    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        
-    }
-};
