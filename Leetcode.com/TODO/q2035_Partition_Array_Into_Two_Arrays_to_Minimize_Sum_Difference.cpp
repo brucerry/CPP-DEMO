bd162d6c4144c4c66ac6190a9c5d3e2c033a5ffd
brucerry <bruce.cheung111@gmail.com>
2022-09-16 21:23:01 +0800

    deleted

--- Leetcode.com/TODO/q2035_Partition_Array_Into_Two_Arrays_to_Minimize_Sum_Difference.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/partition-array-into-two-arrays-to-minimize-sum-difference/
-
-class Solution {
-public:
-    int minimumDifference(vector<int>& nums) {
-        
-    }
-};
