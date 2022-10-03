e7027051fa0238624d80f1e45ade588c3a0746dc
brucerry <bruce.cheung111@gmail.com>
2022-10-03 23:21:28 +0800

    done

--- Leetcode.com/TODO/q2039_The_Time_When_the_Network_Becomes_Idle.cpp
+++
@@ -1,8 +0,0 @@
-// https://leetcode.com/problems/the-time-when-the-network-becomes-idle/
-
-class Solution {
-public:
-    int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
-        
-    }
-};
