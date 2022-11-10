6496903464eff37d79a71597f5c7d5bc25ddfbda
brucerry <bruce.cheung111@gmail.com>
2022-11-10 14:31:28 +0800

    recap

--- Leetcode.com/Repository/1001-2000/q1544_Make_The_String_Great.cpp
+++ Leetcode.com/Repository/1001-2000/q1544_Make_The_String_Great.cpp
@@ -25,11 +25,10 @@ class InPlaceTwoPointers {
 public:
   string makeGood(string s) {
     int end = 0;
-    for (int i = 0; i < s.size(); i++) {
+    for (int i = 0; i < s.size(); i++, end++) {
+      s[end] = s[i];
       if (end and abs(s[end-1] - s[i]) == 32)
-        end--;
-      else
-        s[end++] = s[i];
+        end -= 2;
     }
     return s.substr(0, end);
   }
