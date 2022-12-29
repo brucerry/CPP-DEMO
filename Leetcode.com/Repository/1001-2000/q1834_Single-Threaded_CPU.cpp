fa90148fcdbe5d2937b4e40a5ace63273a97041c
brucerry <bruce.cheung111@gmail.com>
2022-12-29 21:35:54 +0800

    renew solution

--- Leetcode.com/Repository/1001-2000/q1834_Single-Threaded_CPU.cpp
+++ Leetcode.com/Repository/1001-2000/q1834_Single-Threaded_CPU.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
+#include <array>
 using namespace std;
 
 // time: O(n * log(n))
@@ -12,31 +13,25 @@ class Solution {
 public:
   vector<int> getOrder(vector<vector<int>>& tasks) {
     int n = tasks.size();
-    
     for (int i = 0; i < n; i++) {
       tasks[i].emplace_back(i);
     }
-    sort(tasks.begin(), tasks.end()); // start, duration, taskIndex
-    
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> ready; // duration, taskIndex
-    vector<int> order;
-    unsigned int curTime = tasks[0][0], i = 0;
-    while (order.size() < n) {
-      while (i < n and tasks[i][0] <= curTime) {
-        ready.emplace(tasks[i][1], tasks[i][2]);
+    sort(tasks.begin(), tasks.end());
+
+    priority_queue<array<int, 2>, vector<array<int, 2>>, greater<>> minHeap; // procTime, index
+    vector<int> res;
+    long curTime = tasks[0][0], i = 0;
+    while (i < n or minHeap.size()) {
+      curTime = minHeap.empty() ? max(curTime, (long)tasks[i][0]) : curTime;
+      while (i < n and curTime >= tasks[i][0]) {
+        minHeap.push({ tasks[i][1], tasks[i][2] });
         i++;
       }
-      
-      if (ready.size()) {
-        auto [ duration, taskIndex] = ready.top();
-        ready.pop();
-        order.emplace_back(taskIndex);
-        curTime += duration;
-      }
-      else
-        curTime = tasks[i][0];
+      auto [ procTime, index ] = minHeap.top();
+      minHeap.pop();
+      res.emplace_back(index);
+      curTime += procTime;
     }
-    
-    return order;
+    return res;
   }
 };
