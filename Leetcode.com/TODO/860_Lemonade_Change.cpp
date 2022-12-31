aea72716fe040c06a93d70793fa1c002f2a3333d
brucerry <bruce.cheung111@gmail.com>
2022-12-31 19:20:07 +0800

    commit from vscode

--- Leetcode.com/TODO/860_Lemonade_Change.cpp
+++ Leetcode.com/TODO/860_Lemonade_Change.cpp
@@ -1,2 +1,28 @@
 // https://leetcode.com/problems/lemonade-change/
 
+#include <vector>
+using namespace std;
+
+// time: O(n)
+// space: O(1)
+
+class Solution {
+public:
+  bool lemonadeChange(vector<int>& bills) {
+    int five = 0, ten = 0;
+    for (const int& bill : bills) {
+      int change = bill - 5;
+      if (change == 0)
+        five++;
+      else if (change == 5)
+        five--, ten++;
+      else if (ten)
+        five--, ten--;
+      else
+        five -= 3;
+      if (five < 0)
+        return false;
+    }
+    return true;
+  }
+};
