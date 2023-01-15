ad3b842e653060bc4ed380b1e9eda25ad80f7e9d
brucerry <bruce.cheung111@gmail.com>
2023-01-15 14:43:46 +0800

    next level solution

--- Leetcode.com/Repository/2001-3000/q2535_Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array.cpp
+++ Leetcode.com/Repository/2001-3000/q2535_Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array.cpp
@@ -9,14 +9,14 @@ using namespace std;
 class Solution {
 public:
     int differenceOfSum(vector<int>& nums) {
-        int element_sum = 0, digit_sum = 0;
-        for (int num : nums) {
-            element_sum += num;
+        int res = 0;
+        for (int& num : nums) {
+            res += num;
             while (num) {
-                digit_sum += num % 10;
+                res -= num % 10;
                 num /= 10;
             }
         }
-        return abs(element_sum - digit_sum);
+        return res;
     }
 };
