a0c625961a458baf666f94b181434253cb92c7ef
brucerry <bruce.cheung111@gmail.com>
2023-02-07 21:41:28 +0800

    commit

--- Leetcode.com/Repository/0001-1000/904_Fruit_Into_Baskets.cpp
+++ Leetcode.com/Repository/0001-1000/904_Fruit_Into_Baskets.cpp
@@ -11,15 +11,16 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         unordered_map<int, int> umap; // fruit, count
-        int l, r;
-        for (l = 0, r = 0; r < fruits.size(); r++) {
+        int res = 0;
+        for (int l = 0, r = 0; r < fruits.size(); r++) {
             umap[fruits[r]]++;
             if (umap.size() > 2) {
                 if (--umap[fruits[l]] == 0)
                     umap.erase(fruits[l]);
                 l++;
             }
+            res = max(res, r - l + 1);
         }
-        return r - l;
+        return res;
     }
 };
