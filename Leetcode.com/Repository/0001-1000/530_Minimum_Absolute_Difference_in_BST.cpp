24c3fb9ae45d46a161869f52537998f65fa5523b
brucerry <bruce.cheung111@gmail.com>
2023-02-17 21:23:44 +0800

    commit

---
+++ Leetcode.com/Repository/0001-1000/530_Minimum_Absolute_Difference_in_BST.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int getMinimumDifference(TreeNode* root) {
+        int res = INT_MAX, prev = -1;
+        solve(root, res, prev);
+        return res;
+    }
+
+private:
+    void solve(TreeNode* node, int& res, int& prev) {
+        if (node) {
+            solve(node->left, res, prev);
+            if (prev != -1)
+                res = min(res, node->val - prev);
+            prev = node->val;
+            solve(node->right, res, prev);
+        }
+    }
+};
