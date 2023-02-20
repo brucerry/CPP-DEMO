17f4cf7f8d001de8508bbfe205892a451b4157ef
brucerry <bruce.cheung111@gmail.com>
2023-02-20 22:01:33 +0800

    recap

--- Leetcode.com/Repository/0001-1000/035_Search_Insert_Position.cpp
+++ Leetcode.com/Repository/0001-1000/035_Search_Insert_Position.cpp
@@ -8,20 +8,15 @@ using namespace std;
 
 class Solution {
 public:
-  int searchInsert(vector<int>& nums, int target) {
-    int l = 0, r = nums.size() - 1;
-    
-    while (l <= r) {
-      int m = l + ((r - l) >> 1);
-      
-      if (nums[m] < target)
-        l = m + 1;
-      else if (nums[m] > target)
-        r = m - 1;
-      else
-        return m;
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] >= target)
+                r = m;
+            else
+                l = m + 1;
+        }
+        return r;
     }
-    
-    return l;
-  }
 };
