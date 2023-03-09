3f729615c2d1311192c6d613fa99ee0a6264d2db
brucerry <bruce.cheung111@gmail.com>
2023-03-09 21:04:16 +0800

    recap

--- Leetcode.com/Repository/0001-1000/142_Linked_List_Cycle_II.cpp
+++ Leetcode.com/Repository/0001-1000/142_Linked_List_Cycle_II.cpp
@@ -12,24 +12,21 @@ struct ListNode {
 
 class Solution {
 public:
-  ListNode *detectCycle(ListNode *head) {
-    ListNode* slow = head, *fast = head;
-    while (fast and fast->next) {
-      slow = slow->next;
-      fast = fast->next->next;
-      if (slow == fast)
-        break;
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head, *fast = head;
+        while (fast and fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+                break;
+        }
+        if (!fast or !fast->next)
+            return nullptr;
+        slow = head;
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow;
     }
-    
-    if (!fast or !fast->next)
-      return nullptr;
-    
-    slow = head;
-    while (slow != fast) {
-      slow = slow->next;
-      fast = fast->next;
-    }
-    
-    return slow;
-  }
 };
