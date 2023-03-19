f2f3a12a36453cb82b8eff08c6ad50c92aa93c45
brucerry <bruce.cheung111@gmail.com>
2023-03-19 15:57:30 +0800

    recap

--- Leetcode.com/Repository/Blind 75/211_Design_Add_and_Search_Words_Data_Structure.cpp
+++ Leetcode.com/Repository/Blind 75/211_Design_Add_and_Search_Words_Data_Structure.cpp
@@ -4,50 +4,57 @@
 #include <array>
 using namespace std;
 
+struct Trie {
+    Trie* children[26] = {};
+    bool isEnd = false;
+
+    Trie() {}
+
+    void insert(string& word) {
+        Trie* cur = this;
+        for (char c : word) {
+            if (!cur->children[c - 'a'])
+                cur->children[c - 'a'] = new Trie();
+            cur = cur->children[c - 'a'];
+        }
+        cur->isEnd = true;
+    }
+
+    bool search(string& word, Trie* cur, int start) {
+        for (int i = start; i < word.size(); i++) {
+            if (word[i] == '.') {
+                for (int j = 0; j < 26; j++) {
+                    if (cur->children[j] and search(word, cur->children[j], i + 1))
+                        return true;
+                }
+                return false;
+            }
+            else {
+                if (!cur->children[word[i] - 'a'])
+                    return false;
+                cur = cur->children[word[i] - 'a'];
+            }
+        }
+        return cur->isEnd;
+    }
+};
+
 class WordDictionary {
 public:
-  WordDictionary() : isEnd(false) {
-    fill(children.begin(), children.end(), nullptr);
-  }
-  
-  void addWord(string word) {
-    WordDictionary* cur = this;
-    for (const char& c : word) {
-      if (!cur->children[c - 'a'])
-        cur->children[c - 'a'] = new WordDictionary();
-      cur = cur->children[c - 'a'];
-    }
-    cur->isEnd = true;
-  }
-  
-  bool search(string word) {
-    return dfsSearch(word, this, 0);
-  }
-  
-private:
-  bool dfsSearch(string& word, WordDictionary* node, int start) {
-    WordDictionary* cur = node;
-    
-    for (int i = start; i < word.length(); i++) {
-      if (word[i] == '.') {
-        for (int j = 0; j < 26; j++) {
-          if (cur->children[j] and dfsSearch(word, cur->children[j], i + 1))
-            return true;
-        }
-        return false;
-      }
-      else if (!cur->children[word[i] - 'a'])
-        return false;
-      
-      cur = cur->children[word[i] - 'a'];
+    WordDictionary() {
+        root = new Trie();
     }
     
-    return cur->isEnd;
-  }
+    void addWord(string word) {
+        root->insert(word);
+    }
+    
+    bool search(string word) {
+        return root->search(word, root, 0);
+    }
 
 private:
-  bool isEnd;
-  array<WordDictionary*, 26> children;
+    Trie* root;
 };
 
 /**
