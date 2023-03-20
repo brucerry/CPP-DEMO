6d935af65ad875cc085fcd46cb7151a6d9e416fb
brucerry <bruce.cheung111@gmail.com>
2023-03-20 21:33:38 +0800

    add solution

--- Leetcode.com/Repository/0001-1000/605_Can_Place_Flowers.cpp
+++ Leetcode.com/Repository/0001-1000/605_Can_Place_Flowers.cpp
@@ -23,3 +23,19 @@ public:
     return n == 0;
   }
 };
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int size = flowerbed.size();
+        flowerbed.insert(flowerbed.begin(), 0);
+        flowerbed.insert(flowerbed.end(), 0);
+        for (int i = 1; i <= size; i++) {
+            if (flowerbed[i] == 0 and flowerbed[i-1] == 0 and flowerbed[i+1] == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n <= 0;
+    }
+};
