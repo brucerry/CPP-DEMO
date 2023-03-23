87d408299b36cf15a313b5597a104482a2d64e2f
brucerry <bruce.cheung111@gmail.com>
2023-03-23 23:58:12 +0800

    add Union-Find solution

--- Leetcode.com/Repository/1001-2000/q1319_Number_of_Operations_to_Make_Network_Connected.cpp
+++ Leetcode.com/Repository/1001-2000/q1319_Number_of_Operations_to_Make_Network_Connected.cpp
@@ -3,6 +3,42 @@
 #include <vector>
 using namespace std;
 
+// Union-Find solution
+// time: O(n + e)
+// space: O(n)
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if (n > connections.size() + 1)
+            return -1;
+
+        vector<int> parents (n);
+        for (int node = 0; node < n; node++) {
+            parents[node] = node;
+        }
+        for (const auto& c : connections) {
+            n -= unionFind(parents, c[0], c[1]);
+        }
+        return n - 1;
+    }
+
+private:
+    int unionFind(vector<int>& parents, int node1, int node2) {
+        int par1 = find(parents, node1);
+        int par2 = find(parents, node2);
+        if (par1 == par2)
+            return 0;
+        parents[par1] = par2;
+        return 1;
+    }
+
+    int find(vector<int>& parents, int node) {
+        return node == parents[node] ? node : parents[node] = find(parents, parents[node]);
+    }
+};
+
+// DFS solution
 // time: O(n + e)
 // space: O(n + e)
 
