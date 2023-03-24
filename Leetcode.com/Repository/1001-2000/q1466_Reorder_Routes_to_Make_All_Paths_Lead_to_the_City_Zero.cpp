6aeffc405138c302591688e08ef9dd40d7688cb4
brucerry <bruce.cheung111@gmail.com>
2023-03-25 02:15:03 +0800

    recap

--- Leetcode.com/Repository/1001-2000/q1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.cpp
+++ Leetcode.com/Repository/1001-2000/q1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.cpp
@@ -8,22 +8,23 @@ using namespace std;
 
 class Solution {
 public:
-  int minReorder(int n, vector<vector<int>>& connections) {
-    vector<vector<int>> graph (n);
-    for (const auto& conn : connections) {
-      graph[conn[0]].emplace_back(conn[1]);
-      graph[conn[1]].emplace_back(-conn[0]);
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> graph (n);
+        for (const auto& conn : connections) {
+            int from = conn[0], to = conn[1];
+            graph[from].emplace_back(to);
+            graph[to].emplace_back(-from);
+        }
+        return dfs(graph, 0, 0);
     }
-    return dfs(graph, 0, 0);
-  }
 
 private:
-  int dfs(vector<vector<int>>& graph, int prev, int node) {
-    int change = 0;
-    for (const int& neighbor : graph[node]) {
-      if (prev != abs(neighbor))
-        change += dfs(graph, node, abs(neighbor)) + (neighbor > 0);
+    int dfs(vector<vector<int>>& graph, int prev, int cur) {
+        int change = 0;
+        for (const int& neighbor : graph[cur]) {
+            if (prev != abs(neighbor))
+                change += dfs(graph, cur, abs(neighbor)) + (neighbor > 0);
+        }
+        return change;
     }
-    return change;
-  }
 };
