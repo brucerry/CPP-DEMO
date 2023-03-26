4b0ca7d003f10e7876c17bb25a7071a47c60cc30
brucerry <bruce.cheung111@gmail.com>
2023-03-26 15:27:17 +0800

    recap

--- Leetcode.com/Repository/2001-3000/q2360_Longest_Cycle_in_a_Graph.cpp
+++ Leetcode.com/Repository/2001-3000/q2360_Longest_Cycle_in_a_Graph.cpp
@@ -5,49 +5,22 @@ using namespace std;
 
 // time: O(n)
 // space: O(n)
-class Method1 {
-public:
-  int longestCycle(vector<int>& edges) {
-    int n = edges.size();
-    vector<pair<int, int>> distFrom (n, { -1, -1 }); // dist, from
-    
-    int longest = -1;
-    for (int node = 0; node < n; node++) {
-      for (int cur = node, dist = 0; cur != -1; cur = edges[cur]) {
-        auto [ dist_cur, from ] = distFrom[cur];
-        if (dist_cur == -1)
-          distFrom[cur] = { dist++, node };
-        else {
-          if (from == node)
-            longest = max(longest, dist - dist_cur);
-          break;
-        }
-      }
-    }
-    
-    return longest;
-  }
-};
 
-// time: O(n)
-// space: O(n)
-class Method2 {
+class Solution {
 public:
-  int longestCycle(vector<int>& edges) {
-    int n = edges.size();
-    vector<int> dists (n, -1);
-    
-    int longest = -1;
-    for (int node = 0, dist = 0; node < n; node++) {
-      int cur = node, curDist = dist;
-      while (cur != -1 and dists[cur] == -1) {
-        dists[cur] = dist++;
-        cur = edges[cur];
-      }
-      if (cur != -1 and dists[cur] >= curDist)
-        longest = max(longest, dist - dists[cur]);
+    int longestCycle(vector<int>& edges) {
+        int n = edges.size();
+        vector<int> dists (n, -1);
+        int res = -1, dist = 0;
+        for (int node = 0; node < n; node++) {
+            int cur = node, org_dist = dist;
+            while (cur != -1 and dists[cur] == -1) {
+                dists[cur] = dist++;
+                cur = edges[cur];
+            }
+            if (cur != -1 and dists[cur] >= org_dist)
+                res = max(res, dist - dists[cur]);
+        }
+        return res;
     }
-    
-    return longest;
-  }
 };
