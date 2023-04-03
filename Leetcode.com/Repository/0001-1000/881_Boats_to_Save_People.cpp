6cb71740dded8253f624fb3c2d5eafa613d49bd0
brucerry <bruce.cheung111@gmail.com>
2023-04-03 12:25:04 +0800

    recap

--- Leetcode.com/Repository/0001-1000/881_Boats_to_Save_People.cpp
+++ Leetcode.com/Repository/0001-1000/881_Boats_to_Save_People.cpp
@@ -9,20 +9,15 @@ using namespace std;
 
 class Solution {
 public:
-  int numRescueBoats(vector<int>& people, int limit) {
-    sort(people.begin(), people.end());
-    
-    int boat = 0;
-    int l = 0, r = people.size() - 1;
-    while (l <= r) {
-      int sum = people[l] + people[r];
-      if (sum <= limit) {
-        l++;
-      }
-      r--;
-      boat++;
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int res = 0;
+        for (int l = 0, r = people.size() - 1; l <= r; r--) {
+            if (people[l] + people[r] <= limit) {
+                l++;
+            }
+            res++;
+        }
+        return res;
     }
-    
-    return boat;
-  }
 };
