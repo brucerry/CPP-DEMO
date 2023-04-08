8bae0fb4cfc92cfe92a2d8e0bad8690b079a879f
brucerry <bruce.cheung111@gmail.com>
2023-04-08 15:15:50 +0800

    recap

--- Leetcode.com/Repository/Blind 75/133_Clone_Graph.cpp
+++ Leetcode.com/Repository/Blind 75/133_Clone_Graph.cpp
@@ -6,45 +6,43 @@ using namespace std;
 
 class Node {
 public:
-  int val;
-  vector<Node*> neighbors;
-  Node() {
-    val = 0;
-    neighbors = vector<Node*>();
-  }
-  Node(int _val) {
-    val = _val;
-    neighbors = vector<Node*>();
-  }
-  Node(int _val, vector<Node*> _neighbors) {
-    val = _val;
-    neighbors = _neighbors;
-  }
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
 };
 
-// time: O(n + e)
+// time: O(n)
 // space: O(n)
 
 class Solution {
 public:
-  Node* cloneGraph(Node* node) {
-    unordered_map<Node*, Node*> nodeToCopy {
-      { nullptr, nullptr }
-    };
-    return clone(node, nodeToCopy);
-  }
-  
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*, Node*> umap { // real, copy
+            { nullptr, nullptr }
+        };
+        return clone(node, umap);
+    }
+
 private:
-  Node* clone(Node* node, unordered_map<Node*, Node*>& nodeToCopy) {
-    if (nodeToCopy.count(node))
-      return nodeToCopy[node];
-    
-    Node* copy = new Node(node->val);
-    nodeToCopy[node] = copy;
-    
-    for (const auto& neighbor : node->neighbors)
-      copy->neighbors.emplace_back(clone(neighbor, nodeToCopy));
-    
-    return copy;
-  }
+    Node* clone(Node* real, unordered_map<Node*, Node*>& umap) {
+        if (umap.count(real))
+            return umap[real];
+        Node* copy = new Node(real->val);
+        umap[real] = copy;
+        for (const auto& nei : real->neighbors) {
+            copy->neighbors.push_back(clone(nei, umap));
+        }
+        return copy;
+    }
 };
