ac6e5f15509d22015688873e1250aa2411a673e7
brucerry <bruce.cheung111@gmail.com>
2023-04-27 21:39:06 +0800

    commit

---
+++ Leetcode.com/Repository/0001-1000/319_Bulb_Switcher.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/bulb-switcher/
+
+#include <cmath>
+
+// time: O(1)
+// space: O(1)
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
