b7bc18bee23d5978585d601fa12b3ff39f1822be
brucerry <bruce.cheung111@gmail.com>
2023-05-20 17:19:27 +0800

    recap

--- Leetcode.com/Repository/0001-1000/399_Evaluate_Division.cpp
+++ Leetcode.com/Repository/0001-1000/399_Evaluate_Division.cpp
@@ -14,50 +14,48 @@ using namespace std;
 
 class Solution {
 public:
-  vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-    unordered_map<string, vector<pair<string, double>>> graph; // from, (to, val)
-    for (int i = 0; i < equations.size(); i++) {
-      string& from = equations[i][0];
-      string& to = equations[i][1];
-      double val = values[i];
-      graph[from].emplace_back(to, val);
-      graph[to].emplace_back(from, 1.0 / val);
-    }
-    
-    int q = queries.size();
-    vector<double> result (q);
-    for (int i = 0; i < q; i++) {
-      string& from = queries[i][0];
-      string& to = queries[i][1];
-      if (graph.count(from) == 0 or graph.count(to) == 0)
-        result[i] = -1.0;
-      else {
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<string, unordered_map<string, double>> graph;
+        for (int i = 0; i < equations.size(); i++) {
+            string& from = equations[i][0];
+            string& to = equations[i][1];
+            double& val = values[i];
+            graph[from][to] = val;
+            graph[to][from] = 1 / val;
+        }
+
+        vector<double> res;
         unordered_set<string> visited;
-        double ans = 1.0;
-        result[i] = evaluate(graph, from, to, visited, ans) ? ans : -1.0;
-      }
+        for (auto& que : queries) {
+            string& src = que[0];
+            string& dst = que[1];
+            double ans = 1;
+            visited = {};
+            res.emplace_back(dfs(graph, visited, src, dst, ans) ? ans : -1);
+        }
+        return res;
     }
-    
-    return result;
-  }
-  
+
 private:
-  bool evaluate(unordered_map<string, vector<pair<string, double>>>& graph, const string& src, const string& dst, unordered_set<string>& visited, double& ans) {
-    if (src == dst)
-      return true;
-    
-    if (visited.count(src))
-      return false;
-    
-    visited.insert(src);
-    
-    for (const auto& [ neighbor, val ] : graph[src]) {
-      if (evaluate(graph, neighbor, dst, visited, ans)) {
-        ans *= val;
-        return true;
-      }
+    double dfs(unordered_map<string, unordered_map<string, double>>& graph, unordered_set<string>& visited, const string& src, const string& dst, double& ans) {
+        if (graph.count(src) == 0 or graph.count(dst) == 0)
+            return false;
+
+        if (src == dst)
+            return true;
+
+        if (visited.count(src))
+            return false;
+
+        visited.emplace(src);
+        
+        for (const auto& [ nei, val ] : graph[src]) {
+            if (dfs(graph, visited, nei, dst, ans)) {
+                ans *= val;
+                return true;
+            }
+        }
+
+        return false;
     }
-    
-    return false;
-  }
 };
