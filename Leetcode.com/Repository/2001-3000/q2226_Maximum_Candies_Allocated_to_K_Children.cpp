1d17b5337076757e002dd775924c65ef8f093c8f
brucerry <bruce.cheung111@gmail.com>
2023-05-30 22:18:31 +0800

    recap

--- Leetcode.com/Repository/2001-3000/q2226_Maximum_Candies_Allocated_to_K_Children.cpp
+++ Leetcode.com/Repository/2001-3000/q2226_Maximum_Candies_Allocated_to_K_Children.cpp
@@ -3,34 +3,29 @@
 #include <vector>
 using namespace std;
 
-// time: O(n * log(sum(candies) / k))
+// time: O(n * log(1e7))
 // space: O(1)
 
 class Solution {
 public:
-  int maximumCandies(vector<int>& candies, long long k) {    
-    long l = 0, r = 0;
-    for (const int& candy : candies) {
-      r += candy;
+    int maximumCandies(vector<int>& candies, long long k) {
+        int l = 0, r = 1e7;
+        while (l < r) {
+            int m = (l + r + 1) / 2;
+            if (canGet(candies, k, m))
+                l = m;
+            else
+                r = m - 1;
+        }
+        return l;
     }
-    r /= k;
-    
-    while (l < r) {
-      int m = l + ((r - l + 1) >> 1);
-      if (canGet(candies, k, m))
-        l = m;
-      else
-        r = m - 1;
-    }
-    return l;
-  }
-  
+
 private:
-  bool canGet(vector<int>& candies, long long k, int everyoneCanGet) {
-    long childCount = 0;
-    for (const int& candy : candies) {
-      childCount += candy / everyoneCanGet;
+    bool canGet(vector<int>& candies, long long k, int test_max) {
+        long long total = 0;
+        for (int candy : candies) {
+            total += candy / test_max;
+        }
+        return total >= k;
     }
-    return childCount >= k;
-  }
 };
