125e25d9f418856df01a8b38e8327d9366256dec
brucerry <bruce.cheung111@gmail.com>
2023-06-02 21:37:12 +0800

    recap

--- Leetcode.com/Repository/2001-3000/q2101_Detonate_the_Maximum_Bombs.cpp
+++ Leetcode.com/Repository/2001-3000/q2101_Detonate_the_Maximum_Bombs.cpp
@@ -1,7 +1,6 @@
 // https://leetcode.com/problems/detonate-the-maximum-bombs/
 
 #include <vector>
-#include <bitset>
 using namespace std;
 
 // time: O(n^2)
@@ -9,38 +8,37 @@ using namespace std;
 
 class Solution {
 public:
-  int maximumDetonation(vector<vector<int>>& bombs) {
-    int n = bombs.size();
-    
-    vector<vector<int>> graph (n); // node
-    for (int bomb1 = 0; bomb1 < n; bomb1++) {
-      long x1 = bombs[bomb1][0], y1 = bombs[bomb1][1], r1 = bombs[bomb1][2];
-      for (int bomb2 = 0; bomb2 < n; bomb2++) {
-        long x2 = bombs[bomb2][0], y2 = bombs[bomb2][1];
-        if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) <= r1 * r1)
-          graph[bomb1].emplace_back(bomb2);
-      }
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n = bombs.size();
+        vector<vector<int>> graph (n);
+        for (int i = 0; i < n; i++) {
+            long x1 = bombs[i][0], y1 = bombs[i][1], r1 = bombs[i][2];
+            for (int j = 0; j < n; j++) {
+                long x2 = bombs[j][0], y2 = bombs[j][1];
+                if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) <= r1 * r1)
+                    graph[i].emplace_back(j);
+            }
+        }
+
+        int res = 0;
+        vector<int> visited (n);
+        for (int node = 0; node < n; node++) {
+            fill(visited.begin(), visited.end(), 0);
+            res = max(res, dfs(graph, visited, node));
+        }
+
+        return res;
     }
-    
-    int maxDetonation = 0;
-    for (int bomb = 0; bomb < n; bomb++) {
-      maxDetonation = max(maxDetonation, detonate(graph, bomb, bitset<100>() = {}));
-    }
-    
-    return maxDetonation;
-  }
-  
+
 private:
-  int detonate(vector<vector<int>>& graph, int bomb, bitset<100>& detonated) {
-    if (detonated[bomb])
-      return 0;
-    
-    detonated[bomb] = 1;
-    
-    for (const int& neighbor : graph[bomb]) {
-      detonate(graph, neighbor, detonated);
+    int dfs(vector<vector<int>>& graph, vector<int>& visited, int node) {
+        if (visited[node])
+            return 0;
+        visited[node] = 1;
+        int count = 1;
+        for (auto& nei : graph[node]) {
+            count += dfs(graph, visited, nei);
+        }
+        return count;
     }
-    
-    return detonated.count();
-  }
 };
