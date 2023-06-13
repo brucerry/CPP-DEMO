8caa03ddd50909ad9a6490e0e2f0f3fab56dfc98
brucerry <bruce.cheung111@gmail.com>
2023-06-13 21:13:39 +0800

    recap

--- Leetcode.com/Repository/2001-3000/q2352_Equal_Row_and_Column_Pairs.cpp
+++ Leetcode.com/Repository/2001-3000/q2352_Equal_Row_and_Column_Pairs.cpp
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/equal-row-and-column-pairs/
 
 #include <vector>
-#include <unordered_map>
+#include <map>
 using namespace std;
 
 // time: O(n^2)
@@ -9,37 +9,24 @@ using namespace std;
 
 class Solution {
 public:
-  int equalPairs(vector<vector<int>>& grid) {
-    int n = grid.size();
-    
-    unordered_map<vector<int>, int, Hash> rowCount, colCount;
-    for (const auto& row : grid)
-      rowCount[row]++;
-    
-    vector<int> col (n);
-    for (int c = 0; c < n; c++) {
-      for (int r = 0; r < n; r++) {
-        col[r] = grid[r][c];
-      }
-      colCount[col]++;
+    int equalPairs(vector<vector<int>>& grid) {
+        map<vector<int>, int> count;
+        for (auto& row : grid) {
+            count[row]++;
+        }
+
+        int n = grid.size(), res = 0;
+        vector<int> col;
+        for (int c = 0; c < n; c++) {
+            col = {};
+            for (int r = 0; r < n; r++) {
+                col.emplace_back(grid[r][c]);
+            }
+            if (count.count(col)) {
+                res += count[col];
+            }
+        }
+
+        return res;
     }
-    
-    int pairs = 0;
-    for (const auto& [ row, rCount ] : rowCount) {
-      if (colCount.count(row))
-        pairs += rCount * colCount[row];
-    }
-    
-    return pairs;
-  }
-  
-private:
-  struct Hash {
-    int operator()(const vector<int>& nums) const {
-      int result = 0;
-      for (const int& num : nums)
-        result ^= num;
-      return result;
-    }
-  };
 };
