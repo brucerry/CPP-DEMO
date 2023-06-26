8aff0f3aba7df83edc021cd1ecc9e562092d4085
brucerry <bruce.cheung111@gmail.com>
2023-06-26 22:14:07 +0800

    recap, minheap, two pointers

--- Leetcode.com/Repository/2001-3000/q2462_Total_Cost_to_Hire_K_Workers.cpp
+++ Leetcode.com/Repository/2001-3000/q2462_Total_Cost_to_Hire_K_Workers.cpp
@@ -2,40 +2,41 @@
 
 #include <vector>
 #include <queue>
-#include <tuple>
+#include <array>
 using namespace std;
 
 // time: O(n * log(n))
 // space: O(n)
 
 class Solution {
-#define LEFT 0
-#define RIGHT 1
-
 public:
-  long long totalCost(vector<int>& costs, int k, int candidates) {
-    int n = costs.size();
-    
-    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> minheap; // cost, index, side
-    int l = 0, r = n - 1;
-    while (l < candidates and l <= r)
-      minheap.emplace(costs[l], l++, LEFT);
-    while (n - r <= candidates and l <= r)
-      minheap.emplace(costs[r], r--, RIGHT);
-    
-    long result = 0;
-    for (; k and minheap.size(); k--) {
-      auto [ cost, i, side ] = minheap.top();
-      minheap.pop();
-      result += cost;
-      if (l <= r) {
-        if (side == LEFT)
-          minheap.emplace(costs[l], l++, LEFT);
-        else
-          minheap.emplace(costs[r], r--, RIGHT);
-      }
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        priority_queue<array<int, 3>, vector<array<int, 3>>, greater<>> minheap; // cost, i, left/right
+        int n = costs.size(), l = 0, r = n - 1;
+        while (l <= r and candidates) {
+            if (l < r) {
+                minheap.push({ costs[r], r, 1 });
+            }
+            minheap.push({ costs[l], l, 0 });
+            l++;
+            r--;
+            candidates--;
+        }
+
+        long res = 0;
+        while (k) {
+            auto [ cost, i, side ] = minheap.top();
+            minheap.pop();
+            res += cost;
+            k--;
+            if (l <= r) {
+                if (side == 0)
+                    minheap.push({ costs[l], l, side }), l++;
+                else
+                    minheap.push({ costs[r], r, side }), r--;
+            }
+        }
+
+        return res;
     }
-    
-    return result;
-  }
 };
