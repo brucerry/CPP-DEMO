daf8a6d443a031289d3c09dc71b9ef861a17e5d5
brucerry <bruce.cheung111@gmail.com>
2023-07-01 20:25:18 +0800

    dp, bitmask

--- Leetcode.com/Repository/2001-3000/q2305_Fair_Distribution_of_Cookies.cpp
+++ Leetcode.com/Repository/2001-3000/q2305_Fair_Distribution_of_Cookies.cpp
@@ -4,61 +4,36 @@
 using namespace std;
 
 // time: O(k * 2^n * n)
-// space: O(k * 2^n)
+// space: O(2^n)
+
 class Solution {
 public:
-  int distributeCookies(vector<int>& cookies, int k) {
-    int n = cookies.size();
-    
-    vector<int> sums (1 << n);
-    for (int mask = 0; mask < 1 << n; mask++) {
-      for (int i = 0; i < n; i++) {
-        sums[mask] += mask & (1 << i) ? cookies[i] : 0;
-      }
-    }
+    int distributeCookies(vector<int>& cookies, int k) {
+        int n = cookies.size();
 
-    vector<vector<int>> dp (k + 1, vector<int>(1 << n, INT_MAX));
-    dp[0][0] = 0;
-
-    for (int child = 1; child <= k; child++) {
-      for (int mask = 0; mask < 1 << n; mask++) {
-        for (int submask = mask; submask; submask = (submask - 1) & mask) {
-          dp[child][mask] = min(dp[child][mask], max(sums[submask], dp[child - 1][submask ^ mask]));
+        vector<int> sums (1 << n);
+        for (int mask = 0; mask < 1 << n; mask++) {
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i))
+                    sum += cookies[i];
+            }
+            sums[mask] = sum;
         }
-      }
-    }
 
-    return dp.back().back();
-  }
-};
+        vector<int> dp (1 << n, INT_MAX), tmp;
+        dp[0] = 0;
 
-// time: O(k * 2^n * n)
-// space: O(2^n)
-class SpaceOptimized {
-public:
-  int distributeCookies(vector<int>& cookies, int k) {
-    int n = cookies.size();
-    
-    vector<int> sums (1 << n);
-    for (int mask = 0; mask < 1 << n; mask++) {
-      for (int i = 0; i < n; i++) {
-        sums[mask] += mask & (1 << i) ? cookies[i] : 0;
-      }
-    }
-
-    vector<int> dp (1 << n, INT_MAX), tmp;
-    dp[0] = 0;
-
-    for (int child = 1; child <= k; child++) {
-      tmp = dp;
-      for (int mask = 0; mask < 1 << n; mask++) {
-        for (int submask = mask; submask; submask = (submask - 1) & mask) {
-          tmp[mask] = min(tmp[mask], max(sums[submask], dp[submask ^ mask]));
+        for (int child = 1; child <= k; child++) {
+            tmp = dp;
+            for (int mask = 0; mask < 1 << n; mask++) {
+                for (int submask = mask; submask; submask = (submask - 1) & mask) {
+                    tmp[mask] = min(tmp[mask], max(sums[submask], dp[submask ^ mask]));
+                }
+            }
+            dp = tmp;
         }
-      }
-      dp = tmp;
-    }
 
-    return dp.back();
-  }
+        return dp.back();
+    }
 };
