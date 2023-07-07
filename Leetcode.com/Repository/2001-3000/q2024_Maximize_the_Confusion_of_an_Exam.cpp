c5db2a72f39069d6848cf6aa86c735d1198424bc
brucerry <bruce.cheung111@gmail.com>
2023-07-07 21:49:55 +0800

    sliding window

--- Leetcode.com/Repository/2001-3000/q2024_Maximize_the_Confusion_of_an_Exam.cpp
+++ Leetcode.com/Repository/2001-3000/q2024_Maximize_the_Confusion_of_an_Exam.cpp
@@ -1,20 +1,20 @@
-// https://leetcode.com/problems/longest-repeating-character-replacement/
+// https://leetcode.com/problems/maximize-the-confusion-of-an-exam/
 
 #include <string>
 using namespace std;
 
-// time: O(s)
+// time: O(n)
 // space: O(1)
 
 class Solution {
 public:
-    int characterReplacement(string s, int k) {
-        int count[26] {};
+    int maxConsecutiveAnswers(string s, int k) {
+        int count[2] {};
         int res = 0, max_count = 0;
         for (int l = 0, r = 0; r < s.size(); r++) {
-            max_count = max(max_count, ++count[s[r] - 'A']);
+            max_count = max(max_count, ++count[s[r] == 'T']);
             while (r - l + 1 - max_count > k) {
-                count[s[l++] - 'A']--;
+                count[s[l++] == 'T']--;
             }
             res = max(res, r - l + 1);
         }
