89866fab0a28bf3792004dccb273b6f440444267
brucerry <bruce.cheung111@gmail.com>
2023-08-17 21:59:29 +0800

    same dp solution, but getting answer in-place

--- Leetcode.com/Repository/0001-1000/542_01_Matrix.cpp
+++ Leetcode.com/Repository/0001-1000/542_01_Matrix.cpp
@@ -8,33 +8,31 @@ using namespace std;
 
 class Solution {
 public:
-  vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-    int rows = mat.size();
-    int cols = mat[0].size();
-    vector<vector<int>> dist (rows, vector<int>(cols, INT_MAX - 100000));
-    
-    for (int r = 0; r < rows; r++) {
-      for (int c = 0; c < cols; c++) {
-        if (mat[r][c] == 0)
-          dist[r][c] = 0;
-        else {
-          if (r)
-            dist[r][c] = min(dist[r][c], dist[r-1][c] + 1);
-          if (c)
-            dist[r][c] = min(dist[r][c], dist[r][c-1] + 1);
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int rows = mat.size();
+        int cols = mat[0].size();
+        int inf = rows + cols;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (mat[r][c]) {
+                    int left = c - 1 >= 0 ? mat[r][c-1] : inf;
+                    int up = r - 1 >= 0 ? mat[r-1][c] : inf;
+                    mat[r][c] = min(left, up) + 1;
+                }
+            }
         }
-      }
+
+        for (int r = rows - 1; r >= 0; r--) {
+            for (int c = cols - 1; c >= 0; c--) {
+                if (mat[r][c]) {
+                    int right = c + 1 < cols ? mat[r][c+1] : inf;
+                    int down = r + 1 < rows ? mat[r+1][c] : inf;
+                    mat[r][c] = min(mat[r][c], min(right, down) + 1);
+                }
+            }
+        }
+
+        return mat;
     }
-    
-    for (int r = rows - 1; r >= 0; r--) {
-      for (int c = cols - 1; c >= 0; c--) {
-        if (r < rows - 1)
-          dist[r][c] = min(dist[r][c], dist[r+1][c] + 1);
-        if (c < cols - 1)
-          dist[r][c] = min(dist[r][c], dist[r][c+1] + 1);
-      }
-    }
-    
-    return dist;
-  }
 };
