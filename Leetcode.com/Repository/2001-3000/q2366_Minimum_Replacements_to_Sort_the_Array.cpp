f7e2cb09308287a69cf261c27c3d543b4ef47bdf
brucerry <bruce.cheung111@gmail.com>
2023-08-30 23:02:40 +0800

    update algorithm

--- Leetcode.com/Repository/2001-3000/q2366_Minimum_Replacements_to_Sort_the_Array.cpp
+++ Leetcode.com/Repository/2001-3000/q2366_Minimum_Replacements_to_Sort_the_Array.cpp
@@ -8,16 +8,13 @@ using namespace std;
 
 class Solution {
 public:
-  long long minimumReplacement(vector<int>& nums) {
-    long long op = 0;
-    for (int i = nums.size() - 2, prev = nums.back(); i >= 0; i--) {
-      int needOp = nums[i] / prev;
-      if (nums[i] % prev) {
-        needOp++;
-        prev = nums[i] / needOp;
-      }
-      op += needOp - 1;
+    long long minimumReplacement(vector<int>& nums) {
+        long res = 0, bound = nums.back();
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            int count = (nums[i] + bound - 1) / bound;
+            res += count - 1;
+            bound = nums[i] / count;
+        }
+        return res;
     }
-    return op;
-  }
 };
