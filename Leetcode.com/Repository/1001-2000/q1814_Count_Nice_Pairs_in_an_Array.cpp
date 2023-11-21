efd4e606d101090ffc32782cdd2ed6a4955c3b30
brucerry <bruce.cheung111@gmail.com>
2023-11-21 23:42:46 +0800

    update style

--- Leetcode.com/Repository/1001-2000/q1814_Count_Nice_Pairs_in_an_Array.cpp
+++ Leetcode.com/Repository/1001-2000/q1814_Count_Nice_Pairs_in_an_Array.cpp
@@ -4,27 +4,26 @@
 #include <unordered_map>
 using namespace std;
 
-// max(nums) = 10^9
-// time: O(n * log(10^9)) => O(n * 9) => O(n)
+// time: O(n)
 // space: O(n)
 
 class Solution {
 public:
-  int countNicePairs(vector<int>& nums) {
-    unordered_map<int, int> diffCount; // diff, count
-    
-    int nice = 0, mod = 1e9 + 7;
-    for (const int& num : nums) {
-      nice = (nice + diffCount[num - rev(num)]++) % mod;
+    int countNicePairs(vector<int>& nums) {
+        unordered_map<int, int> cnt;
+        int res = 0;
+        for (int num : nums) {
+            res = (res + cnt[num - rev(num)]++) % 1000000007;
+        }
+        return res;
     }
-    return nice;
-  }
-  
-private:
-  int rev(int n) {
-    int result = 0;
-    for (; n; n /= 10)
-      result = result * 10 + n % 10;
-    return result;
-  }
+
+    int rev(int num) {
+        int res = 0;
+        while (num) {
+            res = res * 10 + num % 10;
+            num /= 10;
+        }
+        return res;
+    }
 };
