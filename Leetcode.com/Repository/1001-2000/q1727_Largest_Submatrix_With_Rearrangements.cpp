420522457e2a2c5dba387af65d7e125b6532897a
brucerry <bruce.cheung111@gmail.com>
2023-11-26 18:17:20 +0800

    update algorithm

--- Leetcode.com/Repository/1001-2000/q1727_Largest_Submatrix_With_Rearrangements.cpp
+++ Leetcode.com/Repository/1001-2000/q1727_Largest_Submatrix_With_Rearrangements.cpp
@@ -5,33 +5,26 @@
 using namespace std;
 
 // time: O(r * c * log(c))
-// space: O(c)
+// space: O(1)
 
 class Solution {
 public:
-  int largestSubmatrix(vector<vector<int>>& matrix) {
-    int rows = matrix.size();
-    int cols = matrix[0].size();
-    
-    vector<int> heights (cols);
-    int area = 0;
-    
-    for (int r = 0; r < rows; r++) {
-      for (int c = 0; c < cols; c++) {
-        if (matrix[r][c])
-          heights[c]++;
-        else
-          heights[c] = 0;
-      }
-      
-      vector<int> sortedHeights = heights;
-      sort(sortedHeights.begin(), sortedHeights.end());
-      
-      for (int c = 0; c < cols; c++) {
-        area = max(area, sortedHeights[c] * (cols - c));
-      }
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        for (int r = 1; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (matrix[r][c])
+                    matrix[r][c] = matrix[r-1][c] + 1;
+            }
+        }
+        int res = 0;
+        for (int r = 0; r < rows; r++) {
+            sort(matrix[r].begin(), matrix[r].end());
+            for (int c = 0; c < cols; c++) {
+                res = max(res, matrix[r][c] * (cols - c));
+            }
+        }
+        return res;
     }
-    
-    return area;
-  }
 };
