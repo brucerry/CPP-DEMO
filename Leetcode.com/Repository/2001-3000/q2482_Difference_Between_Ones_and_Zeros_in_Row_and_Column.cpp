143db547e92c23553e82fa3ef2ec2aa467ac9bdc
brucerry <bruce.cheung111@gmail.com>
2023-12-14 22:10:31 +0800

    update style

--- Leetcode.com/Repository/2001-3000/q2482_Difference_Between_Ones_and_Zeros_in_Row_and_Column.cpp
+++ Leetcode.com/Repository/2001-3000/q2482_Difference_Between_Ones_and_Zeros_in_Row_and_Column.cpp
@@ -8,31 +8,22 @@ using namespace std;
 
 class Solution {
 public:
-  vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
-    int rows = grid.size(), cols = grid[0].size();
-    
-    vector<int> onesRow (rows), onesCol (cols);
-    vector<int> zerosRow (rows), zerosCol (cols);
-    
-    for (int r = 0; r < rows; r++) {
-      for (int c = 0; c < cols; c++) {
-        if (grid[r][c]) {
-          onesRow[r]++;
-          onesCol[c]++;
+    vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<int>> row (2, vector<int>(rows));
+        vector<vector<int>> col (2, vector<int>(cols));
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                row[grid[r][c]][r]++;
+                col[grid[r][c]][c]++;
+            }
         }
-        else {
-          zerosRow[r]++;
-          zerosCol[c]++;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                grid[r][c] = row[1][r] + col[1][c] - row[0][r] - col[0][c];
+            }
         }
-      }
+        return grid;
     }
-    
-    for (int r = 0; r < rows; r++) {
-      for (int c = 0; c < cols; c++) {
-        grid[r][c] = onesRow[r] + onesCol[c] - zerosRow[r] - zerosCol[c];
-      }
-    }
-    
-    return grid;
-  }
 };
