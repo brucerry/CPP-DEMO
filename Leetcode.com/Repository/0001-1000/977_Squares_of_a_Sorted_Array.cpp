ba457ce511e204f090b63b9d263004c4430d8eff
brucerry <bruce.cheung111@gmail.com>
2024-03-02 15:52:12 +0800

    update style

--- Leetcode.com/Repository/0001-1000/977_Squares_of_a_Sorted_Array.cpp
+++ Leetcode.com/Repository/0001-1000/977_Squares_of_a_Sorted_Array.cpp
@@ -8,23 +8,15 @@ using namespace std;
 
 class Solution {
 public:
-  vector<int> sortedSquares(vector<int>& nums) {
-    int n = nums.size();
-    
-    vector<int> result (n);
-    int l = 0, r = n - 1, i = n - 1;
-    while (i >= 0) {
-      int sqr_l = nums[l] * nums[l];
-      int sqr_r = nums[r] * nums[r];
-      if (sqr_l > sqr_r) {
-        result[i--] = sqr_l;
-        l++;
-      }
-      else {
-        result[i--] = sqr_r;
-        r--;
-      }
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res (n);
+        for (int i = n - 1, l = 0, r = n - 1; i >= 0; i--) {
+            if (abs(nums[l]) > abs(nums[r]))
+                res[i] = nums[l] * nums[l], l++;
+            else
+                res[i] = nums[r] * nums[r], r--;
+        }
+        return res;
     }
-    return result;
-  }
 };
