f4f7c1d35be4d896c90a60b818785d2f661d9a20
brucerry <bruce.cheung111@gmail.com>
2024-03-31 12:37:41 +0800

    update style

--- Leetcode.com/Repository/2001-3000/q2444_Count_Subarrays_With_Fixed_Bounds.cpp
+++ Leetcode.com/Repository/2001-3000/q2444_Count_Subarrays_With_Fixed_Bounds.cpp
@@ -8,32 +8,22 @@ using namespace std;
 
 class Solution {
 public:
-  long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-    // index 0->min 1->max
-    int found[2] {}, lastIndex[2] = {}, validIndex = -1;
-    long result = 0;
-    
-    for (int i = 0; i < nums.size(); i++) {
-      if (nums[i] < minK or nums[i] > maxK) {
-        found[0] = found[1] = false;
-        validIndex = -1;
-        continue;
-      }
-      if (validIndex == -1 and minK <= nums[i] and nums[i] <= maxK)
-        validIndex = i;
-      if (nums[i] == minK) {
-        found[0] = true;
-        lastIndex[0] = i;
-      }
-      if (nums[i] == maxK) {
-        found[1] = true;
-        lastIndex[1] = i;
-      }
-      if (found[0] and found[1]) {
-        result += min(lastIndex[0], lastIndex[1]) - validIndex + 1;
-      }
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        int lastIndex[2] = { -1, -1 }, validIndex = -1;
+        long res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < minK or nums[i] > maxK) {
+                lastIndex[0] = lastIndex[1] = validIndex = -1;
+                continue;
+            }
+            if (validIndex == -1)
+                validIndex = i;
+            if (nums[i] == minK)
+                lastIndex[0] = i;
+            if (nums[i] == maxK)
+                lastIndex[1] = i;
+            res += max(0, min(lastIndex[0], lastIndex[1]) - validIndex + 1);
+        }
+        return res;
     }
-    
-    return result;
-  }
 };
