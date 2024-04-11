e4e7b1a7b70a6da3323e5ffee144746d82ebe7c8
brucerry <bruce.cheung111@gmail.com>
2024-04-11 22:42:23 +0800

    update style

--- Leetcode.com/Repository/0001-1000/402_Remove_K_Digits.cpp
+++ Leetcode.com/Repository/0001-1000/402_Remove_K_Digits.cpp
@@ -4,28 +4,28 @@
 #include <vector>
 using namespace std;
 
-// time: O(s)
+// time: O(n)
 // space: O(1)
 
 class Solution {
 public:
-  string removeKdigits(string& num, int k) {
-    string result;
-    for (const char& c : num) {
-      while (result.length() and result.back() > c and k) {
-        result.pop_back();
-        k--;
-      }
-      result += c;
+    string removeKdigits(string num, int k) {
+        string res;
+        for (char c : num) {
+            while (res.size() and res.back() > c and k) {
+                res.pop_back();
+                k--;
+            }
+            res += c;
+        }
+        int i = 0;
+        while (i < res.size() and res[i] == '0') {
+            i++;
+        }
+        while (k and res.size()) {
+            res.pop_back();
+            k--;
+        }
+        return i < res.size() ? res.substr(i) : "0";
     }
-    
-    int i = 0;
-    while (i < result.length() and result[i] == '0')
-      i++;
-    
-    while (k-- and result.length())
-      result.pop_back();
-    
-    return i < result.length() ? result.substr(i) : "0";
-  }
 };
