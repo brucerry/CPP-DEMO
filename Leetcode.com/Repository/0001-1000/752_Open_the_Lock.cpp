58fa431651e3310da662cdd2c1f93d9240688cca
brucerry <bruce.cheung111@gmail.com>
2024-04-22 20:14:39 +0800

    update style

--- Leetcode.com/Repository/0001-1000/752_Open_the_Lock.cpp
+++ Leetcode.com/Repository/0001-1000/752_Open_the_Lock.cpp
@@ -6,107 +6,101 @@
 #include <queue>
 using namespace std;
 
-class BFS {
+// BFS
+class Solution {
 public:
-  int openLock(vector<string>& deadends, string& target) {
-    unordered_set<string> visited (deadends.begin(), deadends.end());
-    if (visited.count("0000"))
-      return -1;
-    
-    queue<string> queue;
-    queue.push("0000");
-    
-    int turn = 0;
-    while (queue.size()) {
-      int size = queue.size();
-      
-      while (size--) {
-        string code = queue.front();
-        queue.pop();
-        
-        if (code == target)
-          return turn;
-        
-        for (int i = 0; i < 4; i++) {
-          rotate(code, i, true);
-          if (visited.count(code) == 0) {
-            visited.insert(code);
-            queue.push(code);
-          }
-          rotate(code, i, false);
-          rotate(code, i, false);
-          if (visited.count(code) == 0) {
-            visited.insert(code);
-            queue.push(code);
-          }
-          rotate(code, i, true);
+    int openLock(vector<string>& deadends, string& target) {
+        unordered_set<string> visited (deadends.begin(), deadends.end());
+        if (visited.count("0000"))
+            return -1;
+
+        queue<string> queue;
+        queue.push("0000");
+
+        int turn = 0;
+        while (queue.size()) {
+            int size = queue.size();
+            
+            while (size--) {
+                string code = queue.front();
+                queue.pop();
+                
+                if (code == target)
+                    return turn;
+                
+                for (int i = 0; i < 4; i++) {
+                    rotate(code, i, 1);
+                    if (visited.count(code) == 0) {
+                        visited.insert(code);
+                        queue.push(code);
+                    }
+                    rotate(code, i, -1);
+                    rotate(code, i, -1);
+                    if (visited.count(code) == 0) {
+                        visited.insert(code);
+                        queue.push(code);
+                    }
+                    rotate(code, i, 1);
+                }
+            }
+            turn++;
         }
-      }
-      turn++;
+
+        return -1;
     }
-    
-    return -1;
-  }
   
-private:
-  void rotate(string& code, int i, bool inc) {
-    if (inc)
-      code[i] = code[i] == '9' ? '0' : code[i] + 1;
-    else
-      code[i] = code[i] == '0' ? '9' : code[i] - 1;
-  }
+    void rotate(string& code, int i, int change) {
+        code[i] = (code[i] - '0' + change + 10) % 10 + '0';
+    }
 };
 
-class BidirectionalBFS {
+// Bi-directional BFS
+class Solution {
 public:
-  int openLock(vector<string>& deadends, string& target) {
-    unordered_set<string> visited (deadends.begin(), deadends.end());
-    if (visited.count("0000"))
-      return -1;
-    
-    unordered_set<string> head { "0000" }, tail { target }, tmp, *phead, *ptail;
-    
-    int turns = 0;
-    while (head.size() and tail.size()) {
-      if (head.size() < tail.size()) {
-        phead = &head;
-        ptail = &tail;
-      }
-      else {
-        phead = &tail;
-        ptail = &head;
-      }
-      tmp = {};
-      
-      for (string code : *phead) {
-        if (ptail->count(code))
-          return turns;
+    int openLock(vector<string>& deadends, string& target) {
+        unordered_set<string> visited (deadends.begin(), deadends.end());
+        if (visited.count("0000"))
+            return -1;
 
-        if (visited.count(code))
-          continue;
-        visited.insert(code);
-        
-        for (int i = 0; i < 4; i++) {
-          rotate(code, i, true);
-          tmp.insert(code);
-          rotate(code, i, false);
-          rotate(code, i, false);
-          tmp.insert(code);
-          rotate(code, i, true);
+        unordered_set<string> head { "0000" }, tail { target }, tmp, *phead, *ptail;
+
+        int turns = 0;
+        while (head.size() and tail.size()) {
+            if (head.size() < tail.size()) {
+                phead = &head;
+                ptail = &tail;
+            }
+            else {
+                phead = &tail;
+                ptail = &head;
+            }
+            tmp = {};
+            
+            for (string code : *phead) {
+                if (ptail->count(code))
+                    return turns;
+
+                if (visited.count(code))
+                    continue;
+                visited.insert(code);
+                
+                for (int i = 0; i < 4; i++) {
+                    rotate(code, i, 1);
+                    tmp.insert(code);
+                    rotate(code, i, -1);
+                    rotate(code, i, -1);
+                    tmp.insert(code);
+                    rotate(code, i, 1);
+                }
+            }
+            turns++;
+            *phead = tmp;
         }
-      }
-      turns++;
-      *phead = tmp;
+
+        return -1;
     }
-    
-    return -1;
-  }
-  
-private:
-  void rotate(string& code, int i, bool inc) {
-    if (inc)
-      code[i] = code[i] == '9' ? '0' : code[i] + 1;
-    else
-      code[i] = code[i] == '0' ? '9' : code[i] - 1;
-  }
+
+    void rotate(string& code, int i, int change) {
+        code[i] = (code[i] - '0' + change + 10) % 10 + '0';
+    }
 };
