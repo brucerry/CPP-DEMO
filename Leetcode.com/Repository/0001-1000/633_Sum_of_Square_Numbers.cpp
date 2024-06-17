17928b7a2483cd3b1e5fbff54982ebe9bd590167
brucerry <bruce.cheung111@gmail.com>
2024-06-17 20:29:37 +0800

    20240617 update algorithm

--- Leetcode.com/Repository/0001-1000/633_Sum_of_Square_Numbers.cpp
+++ Leetcode.com/Repository/0001-1000/633_Sum_of_Square_Numbers.cpp
@@ -1,31 +1,22 @@
 // https://leetcode.com/problems/sum-of-square-numbers/
 
-// time: O(sqrt(c) * log(c))
+#include <cmath>
+
+// time: O(log(c))
 // space: O(1)
 
 class Solution {
 public:
-  bool judgeSquareSum(int c) {
-    for (long a = 0; a * a <= c; a++) {
-      if (isPerfectSquare(c - a * a))
-        return true;
+    bool judgeSquareSum(int c) {
+        for (long l = 0, r = sqrt(c); l <= r; ) {
+            long val = l * l + r * r;
+            if (val < c)
+                l++;
+            else if (val > c)
+                r--;
+            else
+                return true;
+        }
+        return false;
     }
-    return false;
-  }
-
-private:
-  bool isPerfectSquare(int b_square) {
-    long l = 0, r = b_square;
-    while (l <= r) {
-      long m = l + (r - l) / 2;
-      long ans = m * m;
-      if (ans < b_square)
-        l = m + 1;
-      else if (ans > b_square)
-        r = m - 1;
-      else
-        return true;
-    }
-    return false;
-  }
 };
